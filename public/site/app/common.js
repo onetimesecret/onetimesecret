f06674c4a8eac6fe64a99e98501174ca51b43360
Christopher Murtagh <chris@blamestella.com>
2011-11-03 17:00:57 -0400

    Pushing template, css and js changes to show/hide the options div

---
+++ public/site/app/common.js
@@ -0,0 +1,11 @@
+function OptionsToggle() {
+	var myElement = document.getElementById("options");
+	if(ele.style.display == "block") {
+    	myElement.style.display = "none";
+		myElement.innerHTML = "show";
+  	}
+	else {
+		myElement.style.display = "block";
+		myElement.innerHTML = "hide";
+	}
+} 
