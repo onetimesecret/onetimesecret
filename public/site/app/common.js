4db42c3cec8a97535ae69c01b75e27c81de49e90
Christopher Murtagh <chris@blamestella.com>
2011-11-03 17:11:09 -0400

    Fixing typo in common.js for options toggle

--- public/site/app/common.js
+++ public/site/app/common.js
@@ -1,11 +1,9 @@
 function OptionsToggle() {
-	var myElement = document.getElementById("options");
-	if(ele.style.display == "block") {
-    	myElement.style.display = "none";
-		myElement.innerHTML = "show";
-  	}
-	else {
-		myElement.style.display = "block";
-		myElement.innerHTML = "hide";
-	}
-} 
+    var myElement = document.getElementById("options");
+    if(myElement.style.display == "block") {
+        myElement.style.display = "none";
+    }
+    else {
+        myElement.style.display = "block";
+    }
+}
