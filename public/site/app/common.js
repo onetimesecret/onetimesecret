06e0133fddeb7f8e46f5245b259093fa017c446b
delano <delano@solutious.com>
2011-11-16 17:52:58 -0500

    Obfuscate email address; some copy fixes

--- public/site/app/common.js
+++ public/site/app/common.js
@@ -8,9 +8,26 @@ function OptionsToggle() {
   }
 }
 
+$(function() {  
+  $.fn.deobfuscate = function() {
+    $(this).each(function(i, el) {
+      var email, subject;
+      email = el.innerHTML;
+      email = email.replace(/ at /gi, "@").replace(/ dot /gi, ".");
+      subject = el.getAttribute('data-subject');
+      subject = subject ? ("?subject=" + subject) : ""
+      email = '<a href="mailto:'+email+subject+'">'+email+'</a>';
+      el.innerHTML = email;
+    });
+    return this;
+  };
+});
+
+
 // COMMON BEHAVIORS
 $(function() {  
   $("#secreturi").focus(function(){
     this.select();
   });
+  $('.email').deobfuscate();
 });
