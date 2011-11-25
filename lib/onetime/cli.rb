55a0d817be06fa495363c44d077c27ad1519bca4
delano <delano@solutious.com>
2011-11-25 18:32:13 -0500

    bin/ots build

--- lib/onetime/cli.rb
+++ lib/onetime/cli.rb
@@ -3,17 +3,14 @@ require 'drydock'
 require 'onetime'
 
 class OT::CLI < Drydock::Command
-    def anything
-      p 1
-    end
-    
-    def register_build
-      begin
-        Onetime::VERSION.increment! argv.first
-        puts Onetime::VERSION
-      rescue => ex
-        puts ex.message
-        exit 1
-      end
+
+  def register_build
+    begin
+      Onetime::VERSION.increment! argv.first
+      puts Onetime::VERSION
+    rescue => ex
+      puts ex.message
+      exit 1
     end
   end
+end
