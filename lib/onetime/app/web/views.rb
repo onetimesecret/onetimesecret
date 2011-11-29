981c42f897743b07a0efb06c36a31c371a990e8f
delano <delano@solutious.com>
2011-11-29 15:15:22 -0500

    Helper cleanup

--- lib/onetime/app/web/views.rb
+++ lib/onetime/app/web/views.rb
@@ -66,43 +66,6 @@ module Onetime
       unless form_fields
       end
     end
-    def baseuri
-      scheme = Onetime.conf[:site][:ssl] ? 'https://' : 'http://'
-      [scheme, Onetime.conf[:site][:host]].join
-    end
-    protected
-    def natural_time(e)
-      return if e.nil?
-      val = Time.now.utc.to_i - e.to_i
-      #puts val
-      if val < 10 
-        result = 'a moment ago'
-      elsif val < 40  
-        result = 'about ' + (val * 1.5).to_i.to_s.slice(0,1) + '0 seconds ago'
-      elsif val < 60 
-        result = 'about a minute ago'
-      elsif val < 60 * 1.3  
-        result = "1 minute ago"
-      elsif val < 60 * 2  
-        result = "2 minutes ago"
-      elsif val < 60 * 50  
-        result = "#{(val / 60).to_i} minutes ago"
-      elsif val < 3600 * 1.4 
-        result = 'about 1 hour ago'
-      elsif val < 3600 * (24 / 1.02) 
-        result = "about #{(val / 60 / 60 * 1.02).to_i} hours ago"
-      elsif val < 3600 * 24 * 1.6
-        result = Time.at(e.to_i).strftime("yesterday").downcase
-      elsif val < 3600 * 24 * 7
-        result = Time.at(e.to_i).strftime("on %A").downcase
-      #elsif val < 3600 * 24 * 11
-      #  result = Time.at(e.to_i).strftime("last %A").downcase
-      else
-        weeks = (val / 3600.0/24.0/7).to_i
-        result = Time.at(e.to_i).strftime("#{weeks} #{'week'.plural(weeks)} ago").downcase
-      end
-      result
-    end
   end
   
   module Views
@@ -205,7 +168,6 @@ module Onetime
             :api => plan.options[:api] ? 'Yes' : 'No'
           }
         end
-        y self[:anonymous]
       end
     end
     class Dashboard < Onetime::View
@@ -214,7 +176,7 @@ module Onetime
         self[:body_class] = :dashboard
         self[:with_anal] = true
         self[:metadata] = cust.metadata.collect { |m| 
-          { :uri => private_secret_uri(m), 
+          { :uri => private_uri(m), 
             :stamp => natural_time(m.updated), 
             :key => m.key,
             :been_shared => m.state?(:shared) }
--- lib/onetime/email.rb
+++ lib/onetime/email.rb
@@ -1,18 +1,27 @@
 require 'mustache'
 
 module Onetime
-  #require 'onetime/app/web/views/helpers'
+  require 'onetime/app/web/views/helpers'
   class Email < Mustache
-    #include Onetime::Views::Helpers
+    include Onetime::Views::Helpers
     self.template_path = './templates/email'
     self.view_namespace = Onetime::Email
     self.view_path = './onetime/email'
-    def initialize *args
+    attr_reader :cust
+    def initialize cust, *args
+      @cust = cust
       init *args if respond_to? :init
+    end
+    def send_email
     end
     class Welcome < OT::Email
       def init
-        self[:poop] = :hihi
+        self[:subject] = "Verify your One-time Secret account"
+        self[:secret] = secret
+        
+      end
+      def verify_uri
+        secret_uri self[:secret]
       end
     end
   end
