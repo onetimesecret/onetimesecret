dcf1724d8d8b19018f23cd1f2d99a53110af92f8
delano <delano@solutious.com>
2011-11-29 04:36:29 -0500

    Removed OT::Email::Views namespace

--- lib/onetime/email.rb
+++ lib/onetime/email.rb
@@ -4,20 +4,15 @@ module Onetime
   #require 'onetime/app/web/views/helpers'
   class Email < Mustache
     #include Onetime::Views::Helpers
-    module Views
-    end
     self.template_path = './templates/email'
-    self.view_namespace = Onetime::Email::Views
-    self.view_path = './onetime/email/views'
+    self.view_namespace = Onetime::Email
+    self.view_path = './onetime/email'
     def initialize *args
       init *args if respond_to? :init
     end
-    module Views
-      class Welcome < OT::Email
-        p self
-        def init
-          self[:poop] = :hihi
-        end
+    class Welcome < OT::Email
+      def init
+        self[:poop] = :hihi
       end
     end
   end
