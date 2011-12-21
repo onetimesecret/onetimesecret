fabed926d09aaf66a9a2a5b03cb049da5e9ed1cb
Christopher Murtagh <chris@blamestella.com>
2011-12-21 00:37:37 -0500

    Adding PHP API class and very simple example usage (bogus API Token)

---
+++ support/clients/api_example_code.php
@@ -0,0 +1,12 @@
+<?php
+
+  include('onetimesecret-api.php');
+  $myOnetime = new OneTimeSecret;
+  $myOnetime->setRecipient('delano@onetimesecret.com');
+  $myOnetime->setTTL(7200);
+  $myOnetime->setCustomerID('chris@onetimesecret.com');
+  $myOnetime->setToken('4dc74a03fwr9aya5qur5wa8vavo4gih1hasj6181');
+  $myResult = $myOnetime->shareSecret("Jazz, jazz and more jazz.", 'thepassword'));
+  print $myOnetime->getSecretURI($myResult);
+  print "\n\n";
+?>
