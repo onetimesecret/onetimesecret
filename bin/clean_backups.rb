4822a7df8841167d17a5a3583bf6ddd2c243faad
delano <delano@solutious.com>
2012-01-06 04:28:52 -0500

    clean_backup script now runs based on age of the files rather than number of files (?)

--- bin/clean_backups.rb
+++ bin/clean_backups.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/ruby
 
+require 'time'
+
 # Config component for s3cmd (we need to create a new bucket)
-KEEPERS=8
 BUCKET='solutious-onetime'
 S3CMD='/usr/bin/s3cmd -c /root/.s3cfg --no-progress'
-TESTMODE=false
-HOSTNAME=`hostname`.chomp.gsub(/[^0-9a-z\.\-\_]/i, '')
+TESTMODE=true
+NOW=TESTMODE ? Time.parse('2012-01-06 09:21 UTC').utc : Time.now.utc
+THRESHOLD=NOW - 3600*3.5  # 3.5 hours ago
+HOSTNAME=`/bin/hostname`.chomp.gsub(/[^0-9a-z\.\-\_]/i, '')
 
 ## DO NOT MODIFY BELOW THIS LINE (UNLESS YOU'RE A COOL WEIGHT-LIFTER)
 require 'syslog'
@@ -15,15 +18,19 @@ def log msg
 end
 def run
   cmd = "#{S3CMD} ls s3://#{BUCKET}/#{HOSTNAME}/"
+  log NOW
   log "Running #{cmd}" 
   log "THIS IS TESTMODE. FILES WILL NOT BE DELETED." if TESTMODE
-  all_backups = TESTMODE ? File.readlines(DATA) : `#{cmd}`.split($/)
-  stale_backups = all_backups[0..-(KEEPERS+1)] # should be 1 or 2 stale backups every time
+  all_backups = file_list
+  stale_backups = all_backups.select { |info|
+    stamp = Time.parse("#{info[0]} UTC")
+    log '%s is old: %s (%s)' % [info[2], stamp.to_i < THRESHOLD.to_i, stamp]
+    (stamp.to_i < THRESHOLD.to_i)
+  }
   log "%d backups, %d stale" % [all_backups.size, stale_backups.size]
   deleted_count = 0
-  stale_backups.each do |line|
-    next unless line.match(/^\d/)
-    date, time, size, name = *line.chomp.split(/\s+/)
+  stale_backups.each do |info|
+    date, size, name = *info
     cmd = "#{S3CMD} del #{name}"
     log cmd
     `#{cmd}` && (deleted_count+=1) unless TESTMODE
@@ -31,7 +38,32 @@ def run
   msg = "Deleted %d backups" % deleted_count
   STDERR.tty? ? STDERR.puts(msg) : log(msg)
 end
-
+def file_list
+  lines = if TESTMODE 
+   %q{
+2012-01-01 04:01    529451   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-04:01:01.bz2
+2012-01-01 05:01    525428   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-05:01:01.bz2
+2012-01-01 06:01    527257   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-06:01:01.bz2
+2012-01-01 07:01    528084   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-07:01:01.bz2
+2012-01-02 08:01    528088   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-08:01:02.bz2
+2012-01-02 09:01    526939   s3://BUCKET/HOSTNAME/FILANAME-2012-01-22-09:01:01.bz2
+2012-01-02 10:01    525594   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-00:01:01.bz2
+2012-01-06 05:01    526257   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-01:01:01.bz2
+2012-01-06 05:31    524568   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-02:01:01.bz2
+2012-01-06 06:01    522502   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-03:01:02.bz2
+2012-01-06 06:31    524214   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-04:01:01.bz2
+2012-01-06 07:01    520998   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-05:01:01.bz2
+2012-01-06 07:31    521343   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-06:01:01.bz2
+2012-01-06 08:01    512873   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-07:01:01.bz2
+2012-01-06 08:31    509614   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-08:01:01.bz2
+2012-01-06 09:01    507133   s3://BUCKET/HOSTNAME/FILANAME-2012-01-06-09:01:01.bz2
+}
+  else
+    `#{cmd}`
+  end
+  # Returns an array of arrays: [DATETIME, SIZE, FILEPATH]
+  lines = lines.split($/).collect{|l| next if l.empty?; l.split(/\s\s+/); }.compact
+end
 begin
   run
 rescue => ex
@@ -39,21 +71,4 @@ rescue => ex
   STDERR.puts ex.backtrace
 end
 
-__END__
-# Output from s3cmd is expected to look like this:
-2011-12-22 04:01    529451   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-04:01:01.bz2
-2011-12-22 05:01    525428   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-05:01:01.bz2
-2011-12-22 06:01    527257   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-06:01:01.bz2
-2011-12-22 07:01    528084   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-07:01:01.bz2
-2011-12-22 08:01    528088   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-08:01:02.bz2
-2011-12-22 09:01    526939   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-09:01:01.bz2
-2011-12-22 10:01    525594   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-10:01:01.bz2
-2011-12-22 11:01    526257   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-11:01:01.bz2
-2011-12-22 12:01    524568   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-12:01:01.bz2
-2011-12-22 13:01    522502   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-13:01:02.bz2
-2011-12-22 14:01    524214   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-14:01:01.bz2
-2011-12-22 15:01    520998   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-15:01:01.bz2
-2011-12-22 16:01    521343   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-16:01:01.bz2
-2011-12-22 17:01    512873   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-17:01:01.bz2
-2011-12-22 18:01    509614   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-18:01:01.bz2
-2011-12-22 19:01    507133   s3://BUCKET/HOSTNAME/FILANAME-2011-12-22-19:01:01.bz2
+
