9754b517739ba5347b921dd1460f4d1db919a2e5
Sankara Shunmugasundaram <sankara@loyal3.com>
2018-07-17 00:12:28 -0700

    updated with appdirect.com email address validation

--- lib/onetime/logic.rb
+++ lib/onetime/logic.rb
@@ -39,7 +39,7 @@ module Onetime
     class Base
       unless defined?(Onetime::Logic::Base::MOBILE_REGEX)
         MOBILE_REGEX = /^\+?\d{9,16}$/
-        EMAIL_REGEX = %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,12})$}i
+        EMAIL_REGEX = %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@appdirect.com$}i
       end
       attr_reader :sess, :cust, :params, :locale, :processed_params, :plan
       def initialize(sess, cust, params=nil, locale=nil)
@@ -250,8 +250,8 @@ module Onetime
       end
       def raise_concerns
         #limit_action :update_account
-        raise_form_error "Not a valid email address" unless valid_email?(@custid)
-        raise_form_error "Not a valid email address" unless OT::Customer.exists?(@custid)
+        raise_form_error "Not a valid AppDirect email address" unless valid_email?(@custid)
+        raise_form_error "Not a valid AppDirect email address" unless OT::Customer.exists?(@custid)
       end
       def process
         cust = OT::Customer.load @custid
