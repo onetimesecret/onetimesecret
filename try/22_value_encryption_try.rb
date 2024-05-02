ec2e9c3d0a32543590a821e610a5ebaf4e0137e9
delano <delano@onetimesecret.com>
2024-05-02 12:15:23 -0700

    Tryouts: fix encrypted value test

--- try/22_value_encryption_try.rb
+++ try/22_value_encryption_try.rb
@@ -20,8 +20,8 @@ s.value
 s = Onetime::Secret.new :shared
 s.encrypt_value 'poop', :key => 'tryouts'
 puts "These values should match character for character. Not sure why they don't :-?"
-s.value
-#=> '\xEF\xDF\xAEt\xAF\xD6\f\x15oZ\x9E\xB8a\xF1\x9E/'
+s.value.gibbler
+#=> 'cf45ffd1d3f709719411ed2d4b185fa09056fb83'
 
 ## Can decrypt a value
 s = Onetime::Secret.new :shared
