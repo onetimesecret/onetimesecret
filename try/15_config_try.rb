ae73d7ba48b89ff843893772af6f41a9f5d37214
delano <delano@onetimesecret.com>
2024-05-20 21:08:34 -0700

    [#321] Removed unnecessary config tryouts

--- try/15_config_try.rb
+++ try/15_config_try.rb
@@ -6,24 +6,6 @@ require_relative '../lib/onetime'
 OT::Config.path = File.join(__dir__, '..', 'etc', 'config.example')
 OT.load! :cli
 
-# sudo mkdir /etc/onetime
-# mkdir ~/.onetime
-# sudo touch /etc/onetime/config
-# touch ~/.onetime/config
-
-## Available config paths in default mode (app)
-Onetime::Config.find_configs
-#=> ["/etc/onetime/config", File.join(OT::HOME, "etc/config")]
-
-## Available config paths in cli mode
-OT.mode = :cli
-Onetime::Config.find_configs
-#=> [File.join(OT.sysinfo.home, ".onetime/config"), "/etc/onetime/config", File.join(OT::HOME, "etc/config")]
-
-## Available config paths in app mode
-OT.mode = :app
-Onetime::Config.find_configs
-#=> ["/etc/onetime/config", File.join(OT::HOME, "etc/config")]
 
 ## Finds a config path
 relative_path = Onetime::Config.path.gsub("#{__dir__}/", '')
