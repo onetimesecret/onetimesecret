8b312b503be2d915d9a85413e9119370428166c5
delano <delano@onetimesecret.com>
2024-06-24 11:03:03 -0700

    [#449] Remove unused custom js functions

--- public/web/js/main.js
+++ public/web/js/main.js
@@ -18,47 +18,11 @@ $(function() {
     });
     return this;
   };
-  $.fn.clearDefault = function(){
-    return this.each(function(){
-      var default_value = $(this).val();
-      $(this).focus(function(){
-        if ($(this).val() == default_value) $(this).val("");
-      });
-      $(this).blur(function(){
-        if ($(this).val() == "") $(this).val(default_value);
-      });
-    });
-  };
-  $.fn.mustache = function (data, partial, stream) {
-    var content = Mustache.to_html(this.html(), data, partial, stream)
-    this.replaceWith(content);
-    $(this)
-  };
 });
 
-function generateEntropy(e) {
-  var obj = $(this);
-  obj.text(obj.attr('alt'));
-  obj.click(function(e) {
-    e.preventDefault();
-  });
-  $.ajax({
-    type: 'POST',
-    url: obj.attr('href'),
-    data: {'shrimp': shrimp},
-    success: function(data, textStatus){
-      window.location.reload()
-    },
-    error: function(){
-      alertify.error("Ooops! There was an error.")
-    }
-  });
-  return e.preventDefault();
-};
 
 // COMMON BEHAVIORS
 $(function() {
-  $('.entropy-generate').click(generateEntropy);
   $('#secreturi').select();
   $(".selectable").click(function(){
     this.select();
