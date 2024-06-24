fa2bc0435451e6be9b5a265a12a7c0b88662f16d
delano <delano@onetimesecret.com>
2024-06-24 07:51:57 -0700

    [#444] Remove cruft from config tryouts

--- try/15_config_try.rb
+++ try/15_config_try.rb
@@ -8,22 +8,9 @@ OT.boot! :cli
 
 @email_address = OT.conf[:emailer][:from]
 
-@truemail_test_config = Truemail::Configuration.new do |config|
-  config.verifier_email = @email_address
-
-  config.whitelisted_emails = [
-    'tryouts+test1@onetimesecret.com',
-    'tryouts+test2@onetimesecret.com'
-  ]
-  config.blacklisted_emails = [
-    'tryouts+test3@onetimesecret.com',
-    'tryouts+test4@onetimesecret.com'
-  ]
-end
 
 ## Finds a config path
-relative_path = Onetime::Config.path.gsub("#{__dir__}/", '')
-relative_path
+Onetime::Config.path.gsub("#{__dir__}/", '')
 #=> "../etc/config.test"
 
 ## Can load config
