a2081bb8d74cf64d210eaabbec737acecb575683
delano <delano@onetimesecret.com>
2024-06-25 11:43:44 -0700

    [#155] Ensure change from #451 carries through
    Making authentication settings available to views (#170). Note that this branch (#155) also made changes to this view base:
    * Uses cust.pending?
    * Removes special referrer cases.

--- lib/onetime/app/web/views/base.rb
+++ lib/onetime/app/web/views/base.rb
@@ -17,6 +17,7 @@ module Onetime
         self[:js], self[:css] = [], []
         self[:is_default_locale] = OT.conf[:locales].first.to_s == locale
         self[:supported_locales] = OT.conf[:locales]
+        self[:authentication] = OT.conf[:site][:authentication]
         self[:description] = i18n[:COMMON][:description]
         self[:keywords] = i18n[:COMMON][:keywords]
         self[:ot_version] = OT::VERSION.inspect
@@ -35,7 +36,7 @@ module Onetime
         self[:jsvars] << jsvar(:custid, cust.custid)
         self[:jsvars] << jsvar(:email, cust.email)
         self[:display_links] = true
-        self[:display_options] = true # sess.authenticated?
+        self[:display_options] = true
         self[:display_recipients] = sess.authenticated?
         self[:display_masthead] = true
         if self[:is_subdomain]
