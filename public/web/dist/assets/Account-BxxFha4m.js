48cdcb81c48886c11bd5442f2555f6ed4bf01411
delano <delano@onetimesecret.com>
2024-07-21 09:23:11 -0700

    [#20240720] Re-add signup form partial

--- public/web/dist/assets/Account-BxxFha4m.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as e,c as o}from"./main-COspXmMH.js";const t={};function n(r,s){return e(),o("div")}const a=c(t,[["render",n]]);export{a as default};
--- public/web/dist/assets/Feedback-B4KGCUeK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./main-COspXmMH.js";const r={};function t(n,a){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
