0450da2da592cccc3548aa9dd49b90a18b353e18
delano <delano@onetimesecret.com>
2024-07-29 21:30:27 -0700

    [#20240729] Update build files

---
+++ public/web/dist/assets/Feedback-JmVJ-l-S.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./main-oYL6ejTX.js";const r={};function t(n,a){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
---
+++ public/web/dist/assets/Private-1_lhm48Q.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./main-oYL6ejTX.js";const t={};function o(n,a){return c(),r("div")}const _=e(t,[["render",o]]);export{_ as default};
