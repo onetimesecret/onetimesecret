1bf688a1e54fdf96a15212c540cb4ef2bbcd19c9
delano <delano@onetimesecret.com>
2024-08-02 15:53:41 -0700

    [#510] Make sure copy button is still visible when pushed

--- src/components/DetailField.vue
+++ src/components/DetailField.vue
@@ -12,17 +12,24 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div class="flex bg-white dark:bg-gray-600 rounded-md overflow-hidden">
-    <div class="w-16 flex-shrink-0 flex items-center justify-end px-3 py-2 bg-gray-100 dark:bg-gray-700 border-r border-gray-200 dark:border-gray-500">
-      <span class="text-sm font-medium text-gray-500 dark:text-gray-300">{{ label }}:</span>
+  <div class="flex bg-white dark:bg-gray-600 rounded-md">
+    <div class="w-16 flex-shrink-0 flex items-center justify-end px-3 py-2 bg-gray-100 dark:bg-gray-700 border-r border-gray-200 dark:border-gray-500" ref="labelContainerRef">
+      <span class="text-sm font-medium text-gray-500 dark:text-gray-300" ref="labelRef">{{ label }}:</span>
     </div>
-    <div class="flex-grow flex items-center justify-between p-3 relative">
-      <span class="text-gray-800 dark:text-gray-200 min-w-0 flex-shrink truncate">
-        {{ value }}<span v-if="appendix" class="text-gray-400">{{ appendix }}</span>
-      </span>
+    <div class="flex-grow flex items-center p-3">
+      <div class="flex-grow min-w-0 mr-2">
+        <span class="text-gray-800 dark:text-gray-200 block truncate">
+          {{ value }}<span v-if="appendix" class="text-gray-400">{{ appendix }}</span>
+        </span>
+      </div>
       <div class="flex-shrink-0">
         <CopyButton :text="value" />
       </div>
     </div>
   </div>
 </template>
+
+
+<style scoped>
+
+</style>
