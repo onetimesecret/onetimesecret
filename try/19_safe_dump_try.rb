4e81c39508832ecbae97d8da74de74e61e2a141c
delano <delano@onetimesecret.com>
2024-08-15 21:10:29 -0700

    [#541] Tryouts reclamation: 19_safe_dump_try.rb
    - Removed irrelevant fields from non-safe list.
    - Updated `safe_dump` behavior to include `role`.
    - Added comments to clarify behavior change as of 2024-08-15 while
    upgrading to new Familia version.
      - Initialization now explicitly sets default `role`.

--- try/19_safe_dump_try.rb
+++ try/19_safe_dump_try.rb
@@ -14,12 +14,12 @@ Familia::Features::SafeDump.safe_dump_fields
 
 ## Implementing models like Customer can define safe dump fields
 Onetime::Customer.safe_dump_fields
-#=> [:custid, :role, :verified, :updated, :created, :stripe_customer_id, :stripe_subscription_id, :stripe_checkout_email, :plan, :secrets_created, :active]
+#=> [:custid, :role, :verified, :last_login, :updated, :created, :stripe_customer_id, :stripe_subscription_id, :stripe_checkout_email, :plan, :secrets_created, :active]
 
 ## Implementing models like Customer can safely dump their fields
 cust = Onetime::Customer.new
 cust.safe_dump
-#=> {:custid=>:anon, :role=>"customer", :verified=>nil, :updated=>nil, :created=>nil, :stripe_customer_id=>nil, :stripe_subscription_id=>nil, :stripe_checkout_email=>nil, :plan=>{:planid=>nil, :source=>"parts_unknown"}, :secrets_created=>0, :active=>false}
+#=> {:custid=>:anon, :role=>"customer", :verified=>nil, :last_login=>nil, :updated=>nil, :created=>nil, :stripe_customer_id=>nil, :stripe_subscription_id=>nil, :stripe_checkout_email=>nil, :plan=>{:planid=>nil, :source=>"parts_unknown"}, :secrets_created=>"", :active=>false}
 
 ## Implementing models like Customer do have other fields
 ## that are by default considered not safe to dump.
@@ -29,18 +29,37 @@ all_non_safe_fields = cust.instance_variables.map { |el|
   el.to_s[1..-1].to_sym # slice off the leading @
 }.sort
 
+p [cust.respond_to?(:suffix), cust.respond_to?(:db), cust.respond_to?(:ttl)]
+
 all_non_safe_fields.sort
-#=> [:cache, :custid, :db, :name, :opts, :parent, :prefix, :redis, :suffix, :ttl]
+#=> [:custid, :custom_domains_list, :metadata_list, :role]
 
 ## Implementing models like Customer can rest assured knowing
 ## any other field not in the safe list will not be dumped.
-cust = Onetime::Customer.new(name: 'Lucy')
+cust = Onetime::Customer.new
+cust.instance_variable_set(:"@haircut", "coupe de longueuil")
+
 all_safe_fields = cust.safe_dump.keys.sort
 
 all_non_safe_fields = cust.instance_variables.map { |el|
   el.to_s[1..-1].to_sym # slice off the leading @
 }.sort
 
+# NOTE: Slight behaviour change as of 2024-08-15 while upgrading to Familia
+# v1.0. The expected return value was `[:custid]` and has been updated to
+# include `:role`. The naming of safe vs non-safe is a bit misleading: the
+# fact that a field is returned as an instance variable just means that a
+# value has been set for that field and has nothing to do with whether or
+# not that field is safe to dump.
+# The reason :role is present is a result of this change where we explicitly
+# set a value for role an initialization time:
+#   class Customer
+#     def init
+#       self.custid ||= :anon
+#       self.role ||= 'customer'
+#     end
+#   end
+
 # Check if any of the non-safe fields are in the safe dump
 all_non_safe_fields & all_safe_fields
-#=> [:custid]
+#=> [:custid, :role]
