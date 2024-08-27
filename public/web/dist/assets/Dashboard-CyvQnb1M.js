234598615e4fa2312e9dfe94a7455bfbdbbbd5e2
delano <delano@onetimesecret.com>
2024-08-26 18:35:27 -0700

    [#20240826] Update build files

---
+++ public/web/dist/assets/Dashboard-CyvQnb1M.js
@@ -0,0 +1,1 @@
+import{a4 as f}from"./main-B2mfUPR_.js";export{f as default};
--- public/web/dist/assets/Shared-BlZ5L_dx.js
+++
@@ -1,1 +0,0 @@
-import{B as e,o as c,c as r}from"./main-Bn6okSDG.js";const o={};function t(n,a){return c(),r("div")}const _=e(o,[["render",t]]);export{_ as default};
