57863fc1b6b8f5680a87776af79a8ad603f04bd3
delano <delano@onetimesecret.com>
2024-09-04 09:14:11 -0700

    [#601] Incorporate Altcha challenge in feedback logic
    - Process `altcha_payload` alongside `msg` parameter
    - Introduce challenges:
      - Check Altcha payload isn't empty
      - Verify Altcha payload using `verify_altcha_payload`
    - Enhance spam filtering with `filter_spam_altcha_payload`
    - Add `secret_key` method for obtaining the Altcha HMAC key
    - Improve feedback security and originality validation

--- lib/onetime/logic/misc.rb
+++ lib/onetime/logic/misc.rb
@@ -1,21 +1,41 @@
 
+require_relative '../../altcha'
 require_relative 'base'
 
 module Onetime::Logic
   module Misc
 
     class ReceiveFeedback < OT::Logic::Base
-      attr_reader :msg
+      attr_reader :msg, :altcha_payload, :verified, :verification_data
+
       def process_params
         @msg = params[:msg].to_s.slice(0, 999)
+        @altcha_payload = params[:altcha].to_s.slice(0, 999)
       end
 
       def raise_concerns
         limit_action :send_feedback
-        raise_form_error "You need an account to do that" if cust.anonymous?
-        if @msg.empty? || @msg =~ /#{Regexp.escape("question or comment")}/
-          raise_form_error "You can be more original than that!"
-        end
+
+        raise_form_error "You can be more original than that!" if @msg.empty?
+        raise_form_error "You need to be carbon-based to do that" if altcha_payload.empty?
+        raise_form_error "Invalid Altcha payload" unless verify_altcha_payload
+      end
+
+      def verify_altcha_payload
+        @verified = Altcha.verify_solution(altcha_payload, secret_key)
+      end
+
+      def filter_spam_altcha_payload
+        @verified, @verification_data = Altcha.verify_server_signature(altcha_payload, secret_key)
+
+        fields_verified = Altcha.verify_fields_hash(
+          params,
+          verification_data.fields,
+          verification_data.fields_hash,
+          'SHA-256'
+        )
+
+        verified && fields_verified
       end
 
       def process
@@ -23,6 +43,10 @@ module Onetime::Logic
         OT.ld [:receive_feedback, msg].inspect
         OT::Feedback.add @msg
         sess.set_info_message "Message received. Send as much as you like!"
+      end
+
+      def secret_key
+        OT.conf.dig(:site, :authenticity, :secret_key) # ALTCHA_HMAC_KEY
       end
     end
 
