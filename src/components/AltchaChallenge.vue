46bfd8e989b5e420c6129c370d935b57697db730
delano <delano@onetimesecret.com>
2024-09-04 09:20:37 -0700

    [#601] Add AltchaChallenge
    - Integrate altcha widget with props for config flexibility
    - Setup state change event handling for challenge verification
    - Utilize ref to interact with altcha widget instance
    - Support optional floating behavior for widget placement

---
+++ src/components/AltchaChallenge.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted, watch } from 'vue';
+// Importing altcha package will introduce a new element <altcha-widget>.
+//
+// See compilerOptions in vite.config.ts.
+import 'altcha';
+
+
+interface Props {
+  payload?: string;
+  isFloating?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isFloating: true,
+});
+
+const emit = defineEmits<{
+  (e: 'update:payload', value: string): void;
+}>();
+
+const altchaWidget = ref<HTMLElement | null>(null);
+const internalValue = ref(props.payload);
+
+watch(internalValue, (v) => {
+  emit('update:payload', v || '');
+});
+
+const onStateChange = (ev: CustomEvent | Event) => {
+  if ('detail' in ev) {
+    const { payload, state } = ev.detail;
+
+    if (state === 'verified' && payload) {
+      internalValue.value = payload;
+    } else {
+      internalValue.value = '';
+    }
+  }
+};
+
+onMounted(() => {
+  if (altchaWidget.value) {
+    altchaWidget.value.addEventListener('statechange', onStateChange);
+  }
+});
+
+onUnmounted(() => {
+  if (altchaWidget.value) {
+    altchaWidget.value.removeEventListener('statechange', onStateChange);
+  }
+});
+</script>
+
+<template>
+  <!-- Configure your `challengeurl` and remove the `test` attribute, see docs: https://altcha.org/docs/website-integration/#using-altcha-widget -->
+  <!-- https://altcha.org/docs/floating-ui/ -->
+  <!-- https://altcha.org/docs/website-integration/ -->
+  <div class="flex ">
+    <altcha-widget ref="altchaWidget"
+                   challengeurl="/api/v2/altcha/challenge"
+                   :floating="isFloating"
+                   floatinganchor="bottom-right"
+                   hidelogo
+                   hidefooter
+                   class="">
+
+    </altcha-widget>
+  </div>
+</template>
+
+<style scoped>
+/* https://altcha.org/docs/widget-customization/ */
+</style>
