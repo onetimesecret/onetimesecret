5f58fe6b00974611dbee76b722bad6c2e8110fe6
delano <delano@onetimesecret.com>
2024-09-06 11:38:35 -0700

    [#608] Enable EmailReceipts for cases without secrets
    - Introduce `message_identifier` for non-secret cases
    - Ensure email receipts are logged with `message_identifier`
    - Improve tracking and error handling for emails with/without secrets

--- lib/onetime/app/mail/base.rb
+++ lib/onetime/app/mail/base.rb
@@ -53,6 +53,12 @@ class Onetime::App
         email_address_obscured = OT::Utils.obscure_email self[:email_address]
         OT.info "Emailing/#{self.token} #{email_address_obscured} [#{self.class}]"
 
+        message_identifier = if self[:secret]
+                    self[:secret].identifier
+                  else
+                    SecureRandom.hex.to_s[0, 24]
+                  end
+
         mailer_response = begin
           # If we have a token of gratitude, we skip the email. There is only one
           # codepath that has a token set. Just keep in mind that this is not an
@@ -67,7 +73,7 @@ class Onetime::App
         internal_emsg = "Cannot send mail: #{ex.message}\n#{ex.backtrace}"
           OT.le internal_emsg
 
-          Onetime::EmailReceipt.create self[:cust].identifier, self[:secret].identifier, internal_emsg
+          Onetime::EmailReceipt.create self[:cust].identifier, message_identifier, internal_emsg
           raise OT::Problem, errmsg
 
         rescue Exception => ex
@@ -75,16 +81,16 @@ class Onetime::App
           OT.le internal_emsg
           OT.le errmsg
 
-          Onetime::EmailReceipt.create self[:cust].identifier, self[:secret].identifier, internal_emsg.to_json
+          Onetime::EmailReceipt.create self[:cust].identifier, message_identifier, internal_emsg.to_json
           raise OT::Problem, errmsg
         end
 
         # Nothing left to do here if we didn't send an email
         return unless mailer_response
 
-        Onetime::EmailReceipt.create self[:cust].identifier, self[:secret].identifier, mailer_response.to_json
+        Onetime::EmailReceipt.create self[:cust].identifier, message_identifier, mailer_response.to_json
 
-        OT.info "[email-sent] to #{email_address_obscured} #{self[:cust].identifier} #{self[:secret].identifier}"
+        OT.info "[email-sent] to #{email_address_obscured} #{self[:cust].identifier} #{message_identifier}"
         mailer_response
       end
     end
