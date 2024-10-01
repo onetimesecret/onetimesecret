b62948de81115f1ba098fbe5735cdabe60d9f1d1
delano <delano@onetimesecret.com>
2024-10-01 11:53:57 -0700

    [#669] Implement quiet layout
    * Wrap layout in BaseLayout component for consistency
    * Apply container and styling for better layout control

--- src/layouts/QuietLayout.vue
+++ src/layouts/QuietLayout.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="">
-    <header>
-      <!-- Your header content -->
-
-    </header>
-    <main>
-      <slot></slot>
-    </main>
-    <footer>
-      <!-- Your footer content -->
-
-    </footer>
-  </div>
+  <BaseLayout v-bind="props">
+    <template #main>
+      <main class="container mx-auto p-4 max-w-2xl" name="QuietLayout">
+        <slot></slot>
+      </main>
+    </template>
+  </BaseLayout>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'QuietLayout'
+<script setup lang="ts">
+import BaseLayout from './BaseLayout.vue';
+import type { Props as BaseLayoutProps } from '@/layouts/BaseLayout.vue';
+
+// Define the props for this layout, extending the BaseLayout props
+interface ImplementingLayoutProps extends BaseLayoutProps {
+  // Add any additional props specific to this layout
+  //additionalProp?: string;
 }
+
+const props = withDefaults(defineProps<ImplementingLayoutProps>(), {
+
+  // You can also add defaults for optional BaseLayout props
+  displayMasthead: false,
+  displayLinks: false,
+  displayFeedback: false,
+  displayVersion: false,
+
+});
 </script>
