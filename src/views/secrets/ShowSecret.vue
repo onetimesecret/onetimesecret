81075cf3f6d8b15c69a1c4c75ec71642d5cf7096
delano <delano@onetimesecret.com>
2024-10-01 11:47:25 -0700

    [#669] Enhance secret view page for loading & onboarding
    * Improved conditional rendering logic for secret details and status.
    * Optimized existing components for a cleaner user interface.
    * Moved onboarding content for first-time recipients to component

--- src/views/secrets/ShowSecret.vue
+++ src/views/secrets/ShowSecret.vue
@@ -1,153 +1,96 @@
 <template>
   <div class="">
-    <div v-if="isLoading">Loading...</div>
-    <!--<div v-else-if="error">Error: {{ error }}</div>-->
-    <div v-else-if="record && details"
-         class="space-y-8">
-      <!-- Owner warning -->
-      <div v-if="!details.verification && details.is_owner && !details.show_secret"
-           class="bg-amber-50 border-l-4 border-amber-400 text-amber-700 p-4 mb-4 dark:bg-amber-900 dark:border-amber-500 dark:text-amber-100">
-        <button type="button"
-                class="float-right hover:text-amber-900 dark:hover:text-amber-50"
-                @click="closeWarning">
-          &times;
-        </button>
-        <strong class="font-medium">{{ $t('web.COMMON.warning') }}</strong>
-        {{ $t('web.shared.you_created_this_secret') }}
-      </div>
-
-      <!-- Owner viewed secret -->
-      <div v-if="!details.verification && details.is_owner && details.show_secret"
-           class="bg-brand-50 border-l-4 border-brand-400 text-brand-700 p-4 mb-4 dark:bg-brand-900 dark:border-brand-500 dark:text-brand-100">
-        <button type="button"
-                class="float-right hover:text-brand-900 dark:hover:text-brand-50"
-                @click="closeWarning">
-          &times;
-        </button>
-        {{ $t('web.shared.viewed_own_secret') }}
-      </div>
-
-      <!-- Secret not yet shown -->
-      <template v-if="!details.show_secret">
-        <BasicFormAlerts :success="success"
-                         :error="error" />
-
-        <p v-if="details.verification && !details.has_passphrase"
-           class="text-md text-gray-600 dark:text-gray-400">
-          {{ $t('web.COMMON.click_to_verify') }}
-        </p>
-        <h2 v-if="details.has_passphrase"
-            class="text-xl font-bold text-gray-800 dark:text-gray-200">
-          {{ $t('web.shared.requires_passphrase') }}
-        </h2>
-
-        <form @submit.prevent="submitForm"
-              class="space-y-4">
-          <input name="shrimp"
-                 type="hidden"
-                 :value="csrfStore.shrimp" />
-          <input name="continue"
-                 type="hidden"
-                 value="true" />
-          <input v-if="details.has_passphrase"
-                 type="password"
-                 v-model="passphrase"
-                 name="passphrase"
-                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-brand-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                 :placeholder="$t('web.COMMON.enter_passphrase_here')" />
-          <button type="submit"
-                  :disabled="isSubmitting"
-                  class="w-full px-6 py-3 text-3xl font-semibold text-white bg-brand-500 rounded-md hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out">
-            {{ isSubmitting ? $t('web.COMMON.submitting') : $t('web.COMMON.click_to_continue') }}
+    <Suspense>
+      <div v-if="isLoading">Loading...</div>
+      <div v-if="record && details"
+           class="space-y-8">
+        <!-- Owner warning -->
+        <div v-if="!details.verification && details.is_owner && !details.show_secret"
+             class="bg-amber-50 border-l-4 border-amber-400 text-amber-700 p-4 mb-4 dark:bg-amber-900 dark:border-amber-500 dark:text-amber-100">
+          <button type="button"
+                  class="float-right hover:text-amber-900 dark:hover:text-amber-50"
+                  @click="closeWarning">
+            &times;
           </button>
-        </form>
-
-        <!-- Secret ready message -->
-        <div v-if="!record.verification">
-          <div class="flex justify-between items-center mb-4">
-            <p class="text-sm text-gray-500 dark:text-gray-400 italic">
-              {{ $t('web.COMMON.careful_only_see_once') }}
-            </p>
-          </div>
-
-          <div class="bg-white dark:bg-gray-900">
-            <div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-              <div class="text-center">
-                <h2
-                    class="text-base font-semibold
-                  text-brandcomp-700 dark:text-brandcomp-600
-                  tracking-wide uppercase opacity-40">
-                  One-Time Secret
-                </h2>
-                <h3
-                    class="mt-1 text-3xl font-extrabold
-                    text-gray-500 dark:text-gray-300
-                    sm:text-3xl sm:tracking-tight lg:text-4xl">
-                  You've got (secret) mail
-                </h3>
-                <p
-                    class="max-w-xl mt-5 mx-auto text-xl
-                    text-gray-400 dark:text-gray-400">
-                  A secure, one-time message awaits you. Before you open it, here's what you should know:
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="bg-white dark:bg-gray-900">
-            <div class="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8">
-              <div class="lg:grid lg:grid-cols-3 lg:gap-8">
-                <div>
-                  <h3 class="text-lg font-medium text-gray-600 dark:text-white">What is this?</h3>
-                  <p class="mt-3 text-base text-gray-400 dark:text-gray-300">
-                    Onetime Secret is a secure way to share sensitive information that self-destructs
-                    after a single viewing.
-                  </p>
-                </div>
-                <div class="mt-8 lg:mt-0">
-                  <h3 class="text-lg font-medium text-gray-600 dark:text-white">Is it secure?</h3>
-                  <p class="mt-3 text-base text-gray-400 dark:text-gray-300">
-                    Yes. After viewing, the secret is permanently deleted from our servers, ensuring your
-                    privacy.
-                  </p>
-                </div>
-                <div class="mt-8 lg:mt-0">
-                  <h3 class="text-lg font-medium text-gray6900 dark:text-white whitespace-nowrap">
-                    What happens next?
-                  </h3>
-                  <p class="mt-3 text-base text-gray-400 dark:text-gray-300">
-                    When ready, click the "View Secret" button at the top of the page to reveal your
-                    one-time message.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <strong class="font-medium">{{ $t('web.COMMON.warning') }}</strong>
+          {{ $t('web.shared.you_created_this_secret') }}
         </div>
-      </template>
 
-      <!-- Secret shown -->
-      <div v-else
-           class="space-y-4">
-        <h2 class="text-gray-600 dark:text-gray-400">{{ $t('web.shared.this_message_for_you') }}</h2>
+        <!-- Owner viewed secret -->
+        <div v-if="!details.verification && details.is_owner && details.show_secret"
+             class="bg-brand-50 border-l-4 border-brand-400 text-brand-700 p-4 mb-4 dark:bg-brand-900 dark:border-brand-500 dark:text-brand-100">
+          <button type="button"
+                  class="float-right hover:text-brand-900 dark:hover:text-brand-50"
+                  @click="closeWarning">
+            &times;
+          </button>
+          {{ $t('web.shared.viewed_own_secret') }}
+        </div>
 
-        <SecretDisplayCase :secret="record"
-                           :details="details" />
+        <!-- Secret confirmation form -->
+        <template v-if="!details.show_secret">
+          <BasicFormAlerts :success="success"
+                           :error="error" />
+
+          <p v-if="details.verification && !details.has_passphrase"
+             class="text-md text-gray-600 dark:text-gray-400">
+            {{ $t('web.COMMON.click_to_verify') }}
+          </p>
+          <h2 v-if="details.has_passphrase"
+              class="text-xl font-bold text-gray-800 dark:text-gray-200">
+            {{ $t('web.shared.requires_passphrase') }}
+          </h2>
+
+          <form @submit.prevent="submitForm"
+                class="space-y-4">
+            <input name="shrimp"
+                   type="hidden"
+                   :value="csrfStore.shrimp" />
+            <input name="continue"
+                   type="hidden"
+                   value="true" />
+            <input v-if="details.has_passphrase"
+                   type="password"
+                   v-model="passphrase"
+                   name="passphrase"
+                   class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-brand-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                   :placeholder="$t('web.COMMON.enter_passphrase_here')" />
+            <button type="submit"
+                    :disabled="isSubmitting"
+                    class="w-full px-6 py-3 text-3xl font-semibold text-white bg-brand-500 rounded-md hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out">
+              {{ isSubmitting ? $t('web.COMMON.submitting') : $t('web.COMMON.click_to_continue') }}
+            </button>
+          </form>
+        </template>
+
+        <!-- Recipient onboarding  -->
+        <SecretRecipientOnboardingContent v-if="!record.verification" />
+
+        <!-- Display the secret -->
+        <div v-else
+             class="space-y-4">
+          <h2 class="text-gray-600 dark:text-gray-400">{{ $t('web.shared.this_message_for_you') }}</h2>
+
+          <SecretDisplayCase :secret="record"
+                             :details="details" />
+        </div>
       </div>
-    </div>
-  </div>
 
-  <UnknownSecret v-if="!record"></UnknownSecret>
+      <UnknownSecret v-else-if="!record || error"></UnknownSecret>
+    </Suspense>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch, computed } from 'vue'
+import BasicFormAlerts from '@/components/BasicFormAlerts.vue'
+import SecretDisplayCase from '@/components/secrets/SecretDisplayCase.vue'
+import SecretRecipientOnboardingContent from '@/components/secrets/SecretRecipientOnboardingContent.vue'
 import { useFetchDataRecord } from '@/composables/useFetchData'
 import { useFormSubmission } from '@/composables/useFormSubmission'
+import { useCsrfStore } from '@/stores/csrfStore'
 import { SecretData, SecretDataApiResponse, SecretDetails } from '@/types/onetime'
 import UnknownSecret from '@/views/secrets/UnknownSecret.vue'
-import SecretDisplayCase from '@/components/secrets/SecretDisplayCase.vue'
-import BasicFormAlerts from '@/components/BasicFormAlerts.vue'
-import { useCsrfStore } from '@/stores/csrfStore';
+import { computed, onMounted, ref, watch } from 'vue'
 
 const csrfStore = useCsrfStore();
 
