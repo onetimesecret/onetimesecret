cc0df256e207d7fc7361258400981a290a0bdd8c
delano <delano@onetimesecret.com>
2024-10-01 11:57:00 -0700

    [#669] Suspence expects a single node

--- src/views/secrets/ShowSecret.vue
+++ src/views/secrets/ShowSecret.vue
@@ -1,82 +1,84 @@
 <template>
   <div class="">
     <Suspense>
-      <div v-if="isLoading">Loading...</div>
-      <div v-if="record && details"
-           class="space-y-8">
-        <!-- Owner warning -->
-        <div v-if="!details.verification && details.is_owner && !details.show_secret"
-             class="bg-amber-50 border-l-4 border-amber-400 text-amber-700 p-4 mb-4 dark:bg-amber-900 dark:border-amber-500 dark:text-amber-100">
-          <button type="button"
-                  class="float-right hover:text-amber-900 dark:hover:text-amber-50"
-                  @click="closeWarning">
-            &times;
-          </button>
-          <strong class="font-medium">{{ $t('web.COMMON.warning') }}</strong>
-          {{ $t('web.shared.you_created_this_secret') }}
-        </div>
-
-        <!-- Owner viewed secret -->
-        <div v-if="!details.verification && details.is_owner && details.show_secret"
-             class="bg-brand-50 border-l-4 border-brand-400 text-brand-700 p-4 mb-4 dark:bg-brand-900 dark:border-brand-500 dark:text-brand-100">
-          <button type="button"
-                  class="float-right hover:text-brand-900 dark:hover:text-brand-50"
-                  @click="closeWarning">
-            &times;
-          </button>
-          {{ $t('web.shared.viewed_own_secret') }}
-        </div>
-
-        <!-- Secret confirmation form -->
-        <template v-if="!details.show_secret">
-          <BasicFormAlerts :success="success"
-                           :error="error" />
-
-          <p v-if="details.verification && !details.has_passphrase"
-             class="text-md text-gray-600 dark:text-gray-400">
-            {{ $t('web.COMMON.click_to_verify') }}
-          </p>
-          <h2 v-if="details.has_passphrase"
-              class="text-xl font-bold text-gray-800 dark:text-gray-200">
-            {{ $t('web.shared.requires_passphrase') }}
-          </h2>
-
-          <form @submit.prevent="submitForm"
-                class="space-y-4">
-            <input name="shrimp"
-                   type="hidden"
-                   :value="csrfStore.shrimp" />
-            <input name="continue"
-                   type="hidden"
-                   value="true" />
-            <input v-if="details.has_passphrase"
-                   type="password"
-                   v-model="passphrase"
-                   name="passphrase"
-                   class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-brand-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                   :placeholder="$t('web.COMMON.enter_passphrase_here')" />
-            <button type="submit"
-                    :disabled="isSubmitting"
-                    class="w-full px-6 py-3 text-3xl font-semibold text-white bg-brand-500 rounded-md hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out">
-              {{ isSubmitting ? $t('web.COMMON.submitting') : $t('web.COMMON.click_to_continue') }}
+      <div>
+        <div v-if="isLoading">Loading...</div>
+        <div v-if="record && details"
+            class="space-y-8">
+          <!-- Owner warning -->
+          <div v-if="!details.verification && details.is_owner && !details.show_secret"
+              class="bg-amber-50 border-l-4 border-amber-400 text-amber-700 p-4 mb-4 dark:bg-amber-900 dark:border-amber-500 dark:text-amber-100">
+            <button type="button"
+                    class="float-right hover:text-amber-900 dark:hover:text-amber-50"
+                    @click="closeWarning">
+              &times;
             </button>
-          </form>
-        </template>
+            <strong class="font-medium">{{ $t('web.COMMON.warning') }}</strong>
+            {{ $t('web.shared.you_created_this_secret') }}
+          </div>
 
-        <!-- Recipient onboarding  -->
-        <SecretRecipientOnboardingContent v-if="!record.verification" />
+          <!-- Owner viewed secret -->
+          <div v-if="!details.verification && details.is_owner && details.show_secret"
+              class="bg-brand-50 border-l-4 border-brand-400 text-brand-700 p-4 mb-4 dark:bg-brand-900 dark:border-brand-500 dark:text-brand-100">
+            <button type="button"
+                    class="float-right hover:text-brand-900 dark:hover:text-brand-50"
+                    @click="closeWarning">
+              &times;
+            </button>
+            {{ $t('web.shared.viewed_own_secret') }}
+          </div>
 
-        <!-- Display the secret -->
-        <div v-else
-             class="space-y-4">
-          <h2 class="text-gray-600 dark:text-gray-400">{{ $t('web.shared.this_message_for_you') }}</h2>
+          <!-- Secret confirmation form -->
+          <template v-if="!details.show_secret">
+            <BasicFormAlerts :success="success"
+                            :error="error" />
 
-          <SecretDisplayCase :secret="record"
-                             :details="details" />
+            <p v-if="details.verification && !details.has_passphrase"
+              class="text-md text-gray-600 dark:text-gray-400">
+              {{ $t('web.COMMON.click_to_verify') }}
+            </p>
+            <h2 v-if="details.has_passphrase"
+                class="text-xl font-bold text-gray-800 dark:text-gray-200">
+              {{ $t('web.shared.requires_passphrase') }}
+            </h2>
+
+            <form @submit.prevent="submitForm"
+                  class="space-y-4">
+              <input name="shrimp"
+                    type="hidden"
+                    :value="csrfStore.shrimp" />
+              <input name="continue"
+                    type="hidden"
+                    value="true" />
+              <input v-if="details.has_passphrase"
+                    type="password"
+                    v-model="passphrase"
+                    name="passphrase"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-brand-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                    :placeholder="$t('web.COMMON.enter_passphrase_here')" />
+              <button type="submit"
+                      :disabled="isSubmitting"
+                      class="w-full px-6 py-3 text-3xl font-semibold text-white bg-brand-500 rounded-md hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition duration-150 ease-in-out">
+                {{ isSubmitting ? $t('web.COMMON.submitting') : $t('web.COMMON.click_to_continue') }}
+              </button>
+            </form>
+          </template>
+
+          <!-- Recipient onboarding  -->
+          <SecretRecipientOnboardingContent v-if="!record.verification" />
+
+          <!-- Display the secret -->
+          <div v-else
+              class="space-y-4">
+            <h2 class="text-gray-600 dark:text-gray-400">{{ $t('web.shared.this_message_for_you') }}</h2>
+
+            <SecretDisplayCase :secret="record"
+                              :details="details" />
+          </div>
         </div>
-      </div>
 
-      <UnknownSecret v-else-if="!record || error"></UnknownSecret>
+        <UnknownSecret v-else-if="!record || error"></UnknownSecret>
+      </div>
     </Suspense>
   </div>
 </template>
