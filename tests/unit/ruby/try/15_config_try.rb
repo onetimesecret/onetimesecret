5d2a0285a279183350c0ab7852536d355ad3993c
delano <delano@onetimesecret.com>
2024-10-05 15:09:21 -0700

    [#709] Move tryouts to new tests home
    Relocate tryout files to a revised directory structure
    for better organization and clarity:
    * New directory is `tests/unit/ruby`.
    * Adjust file paths for dependencies.
    * Update configuration paths for consistency.
    
    Signed-off-by: delano <delano@onetimesecret.com>

---
+++ tests/unit/ruby/try/15_config_try.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+# These tryouts test the configuration functionality of the Onetime application.
+# The Config module is responsible for loading and managing application settings.
+#
+# We're testing various aspects of the configuration, including:
+# 1. Loading and accessing config files
+# 2. Verifying basic configuration structure
+# 3. Checking specific configuration options (e.g., authentication, email settings)
+# 4. Testing utility methods for config key mapping and file existence
+#
+# These tests aim to ensure that the application can correctly load and use
+# its configuration, which is crucial for proper operation and customization.
+
+require 'onetime'
+
+# Use the default config file for tests
+OT::Config.path = File.join(__dir__, '..', 'config.test.yaml')
+OT.boot! :cli
+
+@email_address = OT.conf[:emailer][:from]
+
+
+## Finds a config path
+Onetime::Config.path.gsub("#{__dir__}/", '')
+#=> "../etc/config.test.yaml"
+
+## Can load config
+@config = Onetime::Config.load
+@config.class
+#=> Hash
+
+## Has basic config
+[@config[:site].class, @config[:redis].class]
+#=> [Hash, Hash]
+
+## OT.boot!
+OT.boot! :tryouts
+[OT.mode, OT.conf.class]
+#=> [:tryouts, Hash]
+
+## Has global secret
+Onetime.global_secret.nil?
+#=> false
+
+## Has default global secret
+Onetime.global_secret
+#=> 'SuP0r_53cRU7'
+
+## Config.mapped_key takens an internal key and returns the corresponding external key
+Onetime::Config.mapped_key(:example_internal_key)
+#=> :example_external_key
+
+## Config.mapped_key returns the key itself if it is not in the KEY_MAP
+Onetime::Config.mapped_key(:every_developer_a_key)
+#=> :every_developer_a_key
+
+## Config.find_configs returns an array of paths
+paths = Onetime::Config.find_configs('config.test.yaml')
+path = File.expand_path(File.join(__dir__, '..', 'etc', 'config.test.yaml'))
+paths.include?(path)
+#=> true
+
+## Config.exists? knows if the config file exists
+OT::Config.exists?
+#=> true
+
+## Site has options for authentication
+OT.conf[:site].key? :authentication
+#=> true
+
+## Authentication has options for enabled
+OT.conf[:site][:authentication].key? :enabled
+#=> true
+
+## Authentication is enabled by default
+OT.conf[:site][:authentication][:enabled]
+#=> true
+
+## Signup is enabled by default
+OT.conf[:site][:authentication][:signup]
+#=> true
+
+## Signin is enabled by default
+OT.conf[:site][:authentication][:signin]
+#=> true
+
+## Auto-verification is disabled by default
+OT.conf[:site][:authentication][:autoverify]
+#=> false
+
+## Option for emailer
+OT.conf[:emailer][:from]
+#=> "CHANGEME@example.com"
+
+## An exception is raised if authentication config is missing
+site_authentication = OT.conf[:site].delete(:authentication)
+begin
+  OT::Config.after_load(OT.conf)
+rescue OT::Problem => e
+  puts "Error: #{e}"
+  OT.conf[:site][:authentication] = site_authentication # restore
+  e.message.include?('No `site.authentication` config found')
+end
+#=> true
+
+## An exception is raised if development config is missing
+development = OT.conf.delete(:development)
+begin
+  OT::Config.after_load(OT.conf)
+rescue OT::Problem => e
+  puts "Error: #{e}"
+  OT.conf[:development] = development # restore
+  e.message.include?('No `development` config found')
+end
+#=> true
+
+## An exception is raised if mail config is missing
+mail = OT.conf.delete(:mail)
+begin
+  OT::Config.after_load(OT.conf)
+rescue OT::Problem => e
+  puts "Error: #{e}"
+  OT.conf[:mail] = mail # restore
+  e.message.include?('No `mail` config found')
+end
+#=> true
+
+## (1 of 3) When authentication is disabled, sign-in is disabled regardless of the setting
+OT.conf[:site][:authentication][:enabled] = false
+OT.conf[:site][:authentication][:signin] = true
+OT::Config.after_load(OT.conf)
+OT.conf.dig(:site, :authentication, :signin)
+#=> false
+
+## (2 of 3) When authentication is disabled, sign-up is disabled regardless of the setting
+OT.conf[:site][:authentication][:enabled] = false
+OT.conf[:site][:authentication][:signup] = true
+OT::Config.after_load(OT.conf)
+OT.conf.dig(:site, :authentication, :signup)
+#=> false
+
+## (3 of 3) When authentication is disabled, auto-verify is disabled regardless of the setting
+OT.conf[:site][:authentication][:enabled] = false
+OT.conf[:site][:authentication][:enabled] = true
+OT::Config.after_load(OT.conf)
+OT.conf.dig(:site, :authentication, :signin)
+#=> false
+
+## Default emailer mode is :smtp
+OT.conf[:emailer][:mode]
+#=> :smtp
+
+## Default emailer from address is "CHANGEME@example.com"
+OT.conf[:emailer][:from]
+#=> "CHANGEME@example.com"
+
+## Default emailer fromname is "Jan"
+OT.conf[:emailer][:fromname]
+#=> "Jan"
+
+## Default SMTP host is "localhost"
+OT.conf[:emailer][:host]
+#=> "localhost"
+
+## Default SMTP port is 587
+OT.conf[:emailer][:port]
+#=> 587
+
+## Default SMTP username is "CHANGEME"
+OT.conf[:emailer][:user]
+#=> nil
+
+## Default SMTP password is "CHANGEME"
+OT.conf[:emailer][:pass]
+#=> nil
+
+## Default SMTP auth is "login"
+OT.conf[:emailer][:auth]
+#=> "login"
+
+## Default SMTP TLS is true
+OT.conf[:emailer][:tls]
+#=> true
---
+++ tests/unit/ruby/try/29_customer_domains_try.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+# These tryouts test the customer custom domain relations
+
+
+require 'onetime'
+
+# Load the app
+OT::Config.path = File.join(__dir__, '..', 'config.test.yaml')
+OT.boot! :app
+
+# Setup some variables for these tryouts
+@now = Time.now
+@email_address = "tryouts+#{@now.to_i}@onetimesecret.com"
+@cust = OT::Customer.new @email_address
+
+@valid_domain = 'another.subdomain.onetimesecret.com'
+@input_domains = [
+  'example.com',
+  'subdomain.example.com'
+]
+
+# TRYOUTS
+
+## Customer has a custom domain list
+@cust.custom_domains.class
+#=> Familia::SortedSet
+
+## Customer's custom domain list is stored as a sorted set
+@cust.custom_domains_list.class
+#=> Array
+
+## Customer's custom domain list is empty to start
+@cust.custom_domains.empty?
+#=> true
+
+## Ditto for the sorted set
+@cust.custom_domains_list.empty?
+#=> true
+
+## A customer's custom_domain list updates when a new domain is added
+OT::CustomDomain.create(@valid_domain, @cust.custid)
+@cust.custom_domains.empty?
+#=> false
+
+## A customer's custom_domain list updates when a new domain is added
+custom_domain = OT::CustomDomain.load(@valid_domain, @cust.custid)
+[custom_domain.class, custom_domain.display_domain]
+#=> [OT::CustomDomain, @valid_domain]
+
+## A customer's custom_domain list updates when a new domain is added
+@cust.custom_domains.first
+#=> @valid_domain
+
+## A custom domain has an owner (via model instance)
+custom_domain = OT::CustomDomain.create(@valid_domain, @cust.custid)
+custom_domain.owner?(@cust)
+#=> true
+
+## A custom domain has an owner (via email string)
+custom_domain = OT::CustomDomain.create(@valid_domain, @cust.custid)
+custom_domain.owner?(@cust.custid)
+#=> true
+
+## A custom domain has an owner (nil)
+custom_domain = OT::CustomDomain.create(@valid_domain, @cust.custid)
+custom_domain.owner?(nil)
+#=> false
+
+## A custom domain has an owner (via different email string)
+custom_domain = OT::CustomDomain.create(@valid_domain, @cust.custid)
+custom_domain.owner?('anothercustomer@onetimesecret.com')
+#=> false
+
+## A custom domain has an owner (via different customer)
+cust = OT::Customer.create("anothercustome+#{@now.to_i}r@onetimesecret.com")
+custom_domain = OT::CustomDomain.create(@valid_domain, @cust.custid)
+custom_domain.owner?(cust)
+#=> false
+
+## A customer's custom_domain list updates when a new domain is added
+custom_domain = @cust.custom_domains_list.first
+[custom_domain.class, custom_domain.display_domain]
+#=> [OT::CustomDomain, @valid_domain]
+
+## A customer's custom_domain list updates when an existing domain is removed
+custom_domain = @cust.custom_domains_list.first
+@cust.remove_custom_domain(custom_domain)
+#=> true
+
+## A customer's custom_domain list is empty again after removing a domain
+@cust.custom_domains.empty?
+#=> true
+
+## CustomDomain uses the correct Redis database
+OT::CustomDomain.db
+#=> 6
+
+## CustomDomain has the correct prefix
+OT::CustomDomain.prefix
+#=> :customdomain
+
+## CustomDomain.values is a Familia::SortedSet
+OT::CustomDomain.values.class
+#=> Familia::SortedSet
+
+## CustomDomain.owners is a Familia::HashKey
+OT::CustomDomain.owners.class
+#=> Familia::HashKey
+
+## CustomDomain.values Redis key is correctly prefixed
+OT::CustomDomain.values.rediskey
+#=> "customdomain:values"
+
+## CustomDomain.owners Redis key is correctly prefixed
+OT::CustomDomain.owners.rediskey
+#=> "customdomain:owners"
