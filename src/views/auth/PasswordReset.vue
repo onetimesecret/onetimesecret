8cb6b2d79aef27eb9116562929ad08188bd85233
delano <delano@onetimesecret.com>
2024-10-09 18:07:11 -0700

    [#730] Split password reset into separate components
    - Moved password reset request form to a new component for clarity
    - Adjusted password reset forms to a minimum length of 6 characters
    - Simplifies management and improves code readability

--- src/views/auth/PasswordReset.vue
+++ src/views/auth/PasswordReset.vue
@@ -19,42 +19,6 @@ const props = withDefaults(defineProps<Props>(), {
     Request password reset
   </h3>
 
-  <!--{{^verified}}-->
-  <div class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <p class="mb-4 text-gray-700 dark:text-gray-300">
-      Enter your email address below, and we'll send you instructions to reset your password.
-    </p>
-    <form method="post"
-          id="resetRequestForm">
-      <input type="hidden"
-             name="shrimp"
-             :value="csrfStore.shrimp" />
-
-      <div class="mb-4">
-        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-               for="custidField">
-          Email address
-        </label>
-        <input type="email"
-               name="u"
-               id="custidField"
-               required
-               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-               value="{{form_fields.custid}}"
-               placeholder="your@email.com" />
-      </div>
-      <div class="mb-6"></div>
-      <div class="flex items-center justify-between">
-        <button type="submit"
-                class="bg-brand-500 hover:bg-brand-700 dark:bg-brand-600 dark:hover:bg-brand-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300">
-          Request Reset
-        </button>
-      </div>
-    </form>
-  </div>
-  <!--{{/verified}}-->
-
-  <!--{{#verified}}-->
   <div class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <p class="mb-4 text-gray-700 dark:text-gray-300">
       Please enter your new password below. Make sure it's at least 8 characters long and includes a
@@ -74,9 +38,9 @@ const props = withDefaults(defineProps<Props>(), {
                name="newp"
                id="passField"
                required
-               minlength="8"
+               minlength="6"
                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-               placeholder="••••••••" />
+               placeholder="" />
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
@@ -87,9 +51,9 @@ const props = withDefaults(defineProps<Props>(), {
                name="newp2"
                id="pass2Field"
                required
-               minlength="8"
+               minlength="6"
                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-               placeholder="••••••••" />
+               placeholder="" />
       </div>
       <div id="app"></div>
       <div class="flex items-center justify-between">
@@ -100,7 +64,6 @@ const props = withDefaults(defineProps<Props>(), {
       </div>
     </form>
   </div>
-  <!--{{/verified}}-->
 
   <div class="text-center mt-6">
     <router-link to="/signin"
