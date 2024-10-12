837b8309a1fdb8ee94a74268a8b64a32f6685768
delano <delano@onetimesecret.com>
2024-10-12 11:25:31 -0700

    [#744] Update layout for footer links, C-notice & toggles
    - Improved layout for better responsiveness and organization
    - Combined copyright and version info for consistency
    - Ensured smooth color transitions with `transition-colors`

--- src/components/layout/DefaultFooter.vue
+++ src/components/layout/DefaultFooter.vue
@@ -1,39 +1,65 @@
 <template>
-  <footer class="min-w-[320px] bg-gray-100 dark:bg-gray-800 py-8 overflow-visible w-full">
-    <div class="container mx-auto px-4 max-w-2xl">
+  <footer class="min-w-[320px] bg-gray-100 dark:bg-gray-800 py-8 overflow-visible w-full transition-colors duration-75">
+    <div class="container my-4 mx-auto px-4 max-w-2xl">
       <div v-if="displayLinks"
-           class="grid grid-cols-2 gap-8 mb-8">
+           class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8 py-10">
+
         <!-- Company links -->
         <div class="space-y-4">
           <h3 class="font-semibold text-gray-700 dark:text-gray-300">Company</h3>
           <ul class="space-y-2">
-            <li v-if="plansEnabled && authentication.enabled">
-              <router-link to="/pricing"
-                           class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
-                           aria-label="Onetime Secret Subscription Pricing">Pricing</router-link>
-            </li>
-            <li>
-              <a href="https://github.com/onetimesecret/onetimesecret"
-                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
-                 aria-label="View source code on GitHub"
-                 rel="noopener noreferrer">GitHub</a>
-            </li>
             <li>
               <router-link to="/about"
                            class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
                            aria-label="About Onetime Secret">About</router-link>
+            </li>
+            <li v-if="plansEnabled && authentication.enabled">
+              <router-link to="/pricing"
+                           class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
+                           aria-label="Onetime Secret Subscription Pricing">Pricing</router-link>
             </li>
             <li v-if="supportHost">
               <a :href="`${supportHost}/blog`"
                  class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
                  aria-label="Our blogging website">Blog</a>
             </li>
+            <li>
+              <a href="https://status.onetimesecret.com/"
+                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
+                 aria-label="Check service status"
+                 rel="noopener noreferrer">Status</a>
+            </li>
           </ul>
         </div>
 
-        <!-- Legal & Status links -->
+        <!-- Resources links -->
         <div class="space-y-4">
-          <h3 class="font-semibold text-gray-700 dark:text-gray-300">Legal & Status</h3>
+          <h3 class="font-semibold text-gray-700 dark:text-gray-300">Resources</h3>
+          <ul class="space-y-2">
+            <li>
+              <a href="https://github.com/onetimesecret/onetimesecret"
+                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
+                 aria-label="View source code on GitHub"
+                 rel="noopener noreferrer">GitHub</a>
+            </li>
+            <li v-if="supportHost">
+              <a :href="`${supportHost}/docs`"
+                 aria-label="Our documentation site"
+                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
+                 rel="noopener noreferrer">Docs</a>
+            </li>
+            <li v-if="supportHost">
+              <a :href="`${supportHost}/docs/rest-api`"
+                 aria-label="Our API documentation"
+                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
+                 rel="noopener noreferrer">API</a>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Legal links -->
+        <div class="space-y-4">
+          <h3 class="font-semibold text-gray-700 dark:text-gray-300">Legals</h3>
           <ul class="space-y-2">
             <li>
               <router-link to="/info/privacy"
@@ -50,27 +76,21 @@
                            class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
                            aria-label="View security information">Security</router-link>
             </li>
-            <li>
-              <a href="https://status.onetimesecret.com/"
-                 class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors"
-                 aria-label="Check service status"
-                 rel="noopener noreferrer">Status</a>
-            </li>
           </ul>
         </div>
       </div>
 
-      <div class="flex justify-between items-center pt-4 border-t border-gray-200 dark:border-gray-700">
-        <div v-if="displayVersion" class="text-sm text-gray-500 dark:text-gray-400">
-          v{{ onetimeVersion }}
+      <div class="flex flex-col sm:flex-row justify-between items-center pt-4 border-t border-gray-200 dark:border-gray-700">
+        <div v-if="displayVersion"
+             class="text-sm text-gray-500 dark:text-gray-400">
+          Copyright &copy; 2024 OnetimeSecret.com - v{{ onetimeVersion }}
         </div>
-
         <div class="flex items-center space-x-4">
           <FeedbackToggle v-if="displayFeedback && authentication.enabled" />
           <ThemeToggle />
           <div class="relative z-50"
-              :class="{ 'opacity-60 hover:opacity-100': !isLanguageMenuOpen }"
-              aria-label="Change language">
+               :class="{ 'opacity-60 hover:opacity-100': !isLanguageMenuOpen }"
+               aria-label="Change language">
             <LanguageToggle @menu-toggled="handleMenuToggled" />
           </div>
         </div>
@@ -78,6 +98,7 @@
     </div>
   </footer>
 </template>
+
 
 
 <script setup lang="ts">
