384e762821f1139add3af2149a6f6793e18afa12
delano <delano@onetimesecret.com>
2024-10-13 11:00:40 -0700

    [#749] Add FancyIconLink components with variants
    - Introduce FancyIconLink with basic link functionality for navigation.
    - Add a variant with a color-changing icon for visual emphasis.
    - Includes a glow effect for dynamic UI impact.
    - Enhance accessibility with aria-labels and optional text props.

---
+++ src/components/ctas/FancyIconLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link :to="to"
+               :aria-label="ariaLabel"
+               class="">
+    <FancyIcon :ariaLabel="ariaLabel" />
+    <span v-if="text">{{ text }}</span>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import FancyIcon from '@/components/ctas/FancyIcon.vue';
+
+defineProps<{
+  text?: string;
+  to: string;
+  ariaLabel: string;
+}>();
+</script>
