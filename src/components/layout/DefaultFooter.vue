251a5731c2ba7b43c850a2ef4dc20769904cbabc
delano <delano@onetimesecret.com>
2024-10-15 19:48:09 -0700

    [#767] Update footer
    - Adjust text and icon styles in the footer for better consistency.
    - Correct spacing in import statement for ThemeToggle component.
    - Enhance code readability by reformatting HTML attributes.

--- src/components/layout/DefaultFooter.vue
+++ src/components/layout/DefaultFooter.vue
@@ -22,7 +22,8 @@
               <a :href="`${supportHost}/blog`"
                  class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
                  aria-label="Read our latest blog posts"
-                 target="_blank" rel="noopener noreferrer">Blog</a>
+                 target="_blank"
+                 rel="noopener noreferrer">Blog</a>
             </li>
           </ul>
         </div>
@@ -35,25 +36,29 @@
               <a href="https://github.com/onetimesecret/onetimesecret"
                  class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
                  aria-label="View our source code on GitHub"
-                 target="_blank" rel="noopener noreferrer">GitHub</a>
+                 target="_blank"
+                 rel="noopener noreferrer">GitHub</a>
             </li>
             <li v-if="supportHost">
               <a :href="`${supportHost}/docs`"
                  aria-label="Access our documentation"
                  class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
-                 target="_blank" rel="noopener noreferrer">Docs</a>
+                 target="_blank"
+                 rel="noopener noreferrer">Docs</a>
             </li>
             <li v-if="supportHost">
               <a :href="`${supportHost}/docs/rest-api`"
                  aria-label="Explore our API documentation"
                  class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
-                 target="_blank" rel="noopener noreferrer">API</a>
+                 target="_blank"
+                 rel="noopener noreferrer">API</a>
             </li>
             <li>
               <a href="https://status.onetimesecret.com/"
                  class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
                  aria-label="Check our service status"
-                 target="_blank" rel="noopener noreferrer">Status</a>
+                 target="_blank"
+                 rel="noopener noreferrer">Status</a>
             </li>
           </ul>
         </div>
@@ -92,9 +97,15 @@
              class="flex flex-wrap items-center justify-center sm:justify-end space-x-4 mb-4 sm:mb-0">
 
           <div class="flex items-center space-x-2">
-            <span class="text-sm font-medium text-gray-600 dark:text-gray-300">EU</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-slate-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd" />
+            <span class="text-sm font-medium text-gray-400 dark:text-gray-300">EU</span>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 class="h-5 w-5 text-slate-400"
+                 viewBox="0 0 20 20"
+                 fill="currentColor"
+                 aria-hidden="true">
+              <path fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+                    clip-rule="evenodd" />
             </svg>
           </div>
           <ThemeToggle />
@@ -111,7 +122,7 @@
 import { ref } from 'vue'
 import type { Props as DefaultProps } from '@/layouts/DefaultLayout.vue';
 import FeedbackToggle from '@/components/FeedbackToggle.vue';
-import ThemeToggle from  '@/components/ThemeToggle.vue';
+import ThemeToggle from '@/components/ThemeToggle.vue';
 // Define the props for this layout, extending the DefaultLayout props
 export interface Props extends DefaultProps {
   displayFeedback?: boolean
