3ec944efe2a875368388ff0f26fbb20295815dd4
delano <delano@onetimesecret.com>
2024-10-17 10:41:19 -0700

    [#767] Redesigns signup form and workflow
    * Revamps the signup form with a modern, responsive UI
    * Uses dynamic regions for localization in Signup view

--- src/components/auth/SignUpForm.vue
+++ src/components/auth/SignUpForm.vue
@@ -1,209 +1,116 @@
+<!-- SignUpForm.vue -->
+<template>
+  <form action="/signup"
+        method="POST"
+        class="mt-8 space-y-6">
+    <div class="rounded-md shadow-sm -space-y-px text-lg">
+      <div>
+        <label for="email-address"
+               class="sr-only">Email address</label>
+        <input id="email-address"
+               name="email"
+               type="email"
+               autocomplete="email"
+               required
+               class="relative block w-full rounded-none rounded-t-md appearance-none
+                      text-lg
+                      border border-gray-300
+                      px-3 py-2
+                      placeholder-gray-500 text-gray-900
+                      focus:outline-none focus:ring-brand-500 focus:border-brand-500 focus:z-10
+                      dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                      dark:focus:ring-brand-500 dark:focus:border-brand-500"
+               placeholder="Email address"
+               v-model="email">
+      </div>
+      <div>
+        <label for="password"
+               class="sr-only">Password</label>
+        <input id="password"
+               name="password"
+               type="password"
+               autocomplete="new-password"
+               required
+               class="relative block w-full rounded-none rounded-b-md appearance-none
+                      text-lg
+                      border border-gray-300
+                      px-3 py-2
+                      placeholder-gray-500 text-gray-900
+                      focus:outline-none focus:ring-brand-500 focus:border-brand-500 focus:z-10
+                      dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                      dark:focus:ring-brand-500 dark:focus:border-brand-500"
+               placeholder="Password"
+               v-model="password">
+      </div>
+    </div>
+
+    <div class="flex items-center justify-between">
+      <div class="flex items-center text-lg">
+        <input id="terms-agreement"
+               name="terms-agreement"
+               type="checkbox"
+               required
+               class="h-4 w-4 rounded
+                      border-gray-300
+                      text-brand-600
+                      focus:ring-brand-500
+                      dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-brand-500"
+               v-model="termsAgreed">
+        <label for="terms-agreement"
+               class="ml-2 block text-sm text-gray-900 dark:text-gray-300">
+          I agree to the
+          <a href="#"
+             class="font-medium text-brand-600 hover:text-brand-500
+                    dark:text-brand-500 dark:hover:text-brand-400">Terms
+            of Service</a>
+          and
+          <a href="#"
+             class="font-medium text-brand-600 hover:text-brand-500
+                    dark:text-brand-500 dark:hover:text-brand-400">Privacy
+            Policy</a>
+        </label>
+      </div>
+    </div>
+
+    <div>
+      <button type="submit"
+              class="group relative w-full flex justify-center
+                     rounded-md
+                     py-2 px-4
+                     text-lg font-medium text-white
+                     border border-transparent
+                     bg-brand-600 hover:bg-brand-700
+                     focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500
+                     dark:bg-brand-600 dark:hover:bg-brand-700 dark:focus:ring-offset-gray-800">
+        Sign up
+      </button>
+    </div>
+  </form>
+</template>
+
+
 <!-- signup_form_basic.vue -->
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useCsrfStore } from '@/stores/csrfStore';
-
-const csrfStore = useCsrfStore();
+import { Jurisdiction } from '@/types/onetime';
+//import { useCsrfStore } from '@/stores/csrfStore';
+//
+//const csrfStore = useCsrfStore();
 
 export interface Props {
   enabled?: boolean;
   planid?: string;
+  jurisdiction?: Jurisdiction
 }
 
-const props = withDefaults(defineProps<Props>(), {
+withDefaults(defineProps<Props>(), {
   enabled: true,
   planid: 'basic',
 })
 
 const email = ref('');
 const password = ref('');
-const confirmPassword = ref('');
-const skill = ref('');
-const passwordFieldType = ref('password');
-const confirmPasswordFieldType = ref('password');
 
-const togglePasswordVisibility = (field: 'password' | 'confirmPassword') => {
-  if (field === 'password') {
-    passwordFieldType.value = passwordFieldType.value === 'password' ? 'text' : 'password';
-  } else {
-    confirmPasswordFieldType.value = confirmPasswordFieldType.value === 'password' ? 'text' : 'password';
-  }
-};
+const termsAgreed = ref(false);
 
 </script>
-
-<template>
-
-  <form action="/signup"
-        method="POST"
-        class="max-w-md mx-auto bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
-    <input type="hidden"
-           name="utf8"
-           value="✓" />
-    <input type="hidden"
-           name="shrimp"
-           :value="csrfStore.shrimp" />
-    <input type="hidden"
-           name="planid"
-           :value="props.planid" />
-
-    <fieldset>
-      <div class="mb-4 relative">
-        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-               for="custidField">
-          {{ $t('web.COMMON.field_email') }}
-        </label>
-        <div class="relative">
-          <input v-model="email"
-                 type="email"
-                 name="u"
-                 id="custidField"
-                 tabindex="1"
-                 class="w-full pl-10 pr-3 py-2 text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-700 border rounded-md transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brandcomp-500 invalid:border-red-500"
-                 :placeholder="$t('web.COMMON.email_placeholder')"
-                 autocomplete="email"
-                 aria-required="true" />
-          <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
-               fill="none"
-               stroke="currentColor"
-               viewBox="0 0 24 24"
-               xmlns="http://www.w3.org/2000/svg"
-               width="20"
-               height="20">
-            <path stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207">
-            </path>
-          </svg>
-        </div>
-      </div>
-
-      <div class="mb-4 relative">
-        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-               for="passField">
-          {{ $t('web.COMMON.field_password') }}
-        </label>
-        <div class="relative">
-          <input v-model="password"
-                 :type="passwordFieldType"
-                 name="p"
-                 id="passField"
-                 tabindex="2"
-                 class="w-full pl-10 pr-10 py-2 text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-700 border rounded-md transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brandcomp-500"
-                 :placeholder="$t('web.COMMON.password_placeholder')"
-                 autocomplete="new-password"
-                 aria-required="true" />
-          <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
-               fill="none"
-               stroke="currentColor"
-               viewBox="0 0 24 24"
-               xmlns="http://www.w3.org/2000/svg"
-               width="20"
-               height="20">
-            <path stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z">
-            </path>
-          </svg>
-          <button type="button"
-                  @click="togglePasswordVisibility('password')"
-                  class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600">
-            <svg class="h-5 w-5"
-                 fill="none"
-                 stroke="currentColor"
-                 viewBox="0 0 24 24"
-                 xmlns="http://www.w3.org/2000/svg"
-                 width="20"
-                 height="20">
-              <path stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              <path stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-              </path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      <div class="mb-4 relative">
-        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-               for="pass2Field">
-          {{ $t('web.COMMON.field_password2') }}
-        </label>
-        <div class="relative">
-          <input v-model="confirmPassword"
-                 :type="confirmPasswordFieldType"
-                 name="p2"
-                 id="pass2Field"
-                 tabindex="3"
-                 class="w-full pl-10 pr-10 py-2 text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-700 border rounded-md transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brandcomp-500"
-                 :placeholder="$t('web.COMMON.confirm_password_placeholder')"
-                 autocomplete="new-password"
-                 aria-required="true" />
-          <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
-               fill="none"
-               stroke="currentColor"
-               viewBox="0 0 24 24"
-               xmlns="http://www.w3.org/2000/svg"
-               width="20"
-               height="20">
-            <path stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z">
-            </path>
-          </svg>
-          <button type="button"
-                  @click="togglePasswordVisibility('confirmPassword')"
-                  class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600">
-            <svg class="h-5 w-5"
-                 fill="none"
-                 stroke="currentColor"
-                 viewBox="0 0 24 24"
-                 xmlns="http://www.w3.org/2000/svg"
-                 width="20"
-                 height="20">
-              <path stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              <path stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-              </path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      <div class="mb-4 hidden">
-        <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-               for="skillTest">
-          Skill
-        </label>
-        <input v-model="skill"
-               type="text"
-               name="skill"
-               id="skillTest"
-               class="w-full px-3 py-2 text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-700 border rounded-md transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brandcomp-500"
-               :placeholder="$t('web.COMMON.skill_placeholder')"
-               autocomplete="off" />
-      </div>
-
-      <div class="flex items-center justify-between">
-        <button type="submit"
-                tabindex="4"
-                class="px-4 py-2 font-bold text-white bg-brand-500 rounded-md hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-brandcomp-500 focus:ring-offset-2 dark:bg-brand-600 dark:hover:bg-brand-700 transition-colors duration-300 ease-in-out transform hover:scale-105">
-          {{ $t('web.COMMON.button_create_account') }}
-        </button>
-      </div>
-    </fieldset>
-  </form>
-</template>
--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,47 +1,79 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-
+<!-- Signup.vue -->
 <template>
-  <div>
-    <section class="mb-8">
-      <h1 class="font-semibold mb-6 text-gray-900 dark:text-gray-100">Signup</h1>
-      <h3 class=" font-semibold mb- text-gray-900 dark:text-gray-100">
-        Our {{ currentPlan.options.name }} plan! You get:
-      </h3>
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div class="flex flex-col items-center">
+        <Icon :icon="currentRegion.icon" class="h-48 w-48 text-brand-600 dark:text-brand-400 mb-32" aria-hidden="true" />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+          Create your account
+        </h2>
+        <p class="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+          Create your account in {{ currentRegion.display_name }}
+        </p>
+      </div>
 
-      <ul class="list-disc pl-6 text-gray-700 dark:text-gray-300">
-        <li>secrets that live up to <span class="font-bold text-brand-600 dark:text-brand-400">{{currentPlan.options.ttl/3600/24}} days</span>.</li>
-        <li v-if="currentPlan.options.email">to send secret links via <span class="font-bold text-brand-600 dark:text-brand-400">email</span>.</li>
-        <li v-if="currentPlan.options.api">access to the <a :href="`${supportHost}/docs/rest-api`" class="font-bold text-brand-600 dark:text-brand-400 hover:underline">API</a>.</li>
-      </ul>
-    </section>
+      <div class="mt-8 space-y-6">
+        <SignUpForm :planid="currentPlanId"
+                    :region="currentRegion" />
+        <div class="relative">
+          <div class="absolute inset-0 flex items-center">
+            <div class="w-full border-t border-gray-300 dark:border-gray-700"></div>
+          </div>
+          <div class="relative flex justify-center text-sm">
+            <span class="px-2 bg-gray-100 dark:bg-gray-900 text-gray-500 dark:text-gray-400">
+              Or continue with
+            </span>
+          </div>
+        </div>
 
-    <PlansElevateCta />
+        <div class="grid grid-cols-3 gap-3">
+          <button v-for="provider in socialProviders"
+                  :key="provider.name"
+                  class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm bg-white dark:bg-gray-800 text-sm font-medium text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors duration-200">
+            <span class="sr-only">Sign up with {{ provider.name }}</span>
+            <Icon :icon="provider.icon"
+                  class="h-5 w-5"
+                  aria-hidden="true" />
+          </button>
+        </div>
 
-    <SignUpForm :planid="currentPlanId" />
-
-    <div class="mt-6 text-center">
-      <router-link to="/signin" class="text-sm text-gray-600 dark:text-gray-400 hover:underline">
-        Already have an account? <strong class="font-medium text-gray-900 dark:text-gray-200">Sign In</strong>
-      </router-link>
+        <div class="flex items-center justify-center">
+          <router-link to="/signin"
+                       class="font-medium text-brand-600 dark:text-brand-400 hover:text-brand-500 dark:hover:text-brand-300 transition-colors duration-200">
+            Already have an account? Sign in
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+
 <script setup lang="ts">
-import PlansElevateCta from '@/components/ctas/PlansElevateCta.vue';
+import { ref, computed } from 'vue';
+import { Icon } from '@iconify/vue';
 import SignUpForm from '@/components/auth/SignUpForm.vue';
-import { useWindowProp } from '@/composables/useWindowProps';
+import { useWindowProps } from '@/composables/useWindowProps';
+import type { Jurisdiction } from '@/types/onetime';
 
-// This prop is passed from vue-router b/c the route has `prop: true`.
-interface Props {
-  planCode?: string
-}
+const { regions_enabled: regionsEnabled, regions } = useWindowProps(['regions_enabled', 'regions']);
 
-const props = defineProps<Props>()
+const currentPlanId = ref('basic');
 
-const currentPlanId = props.planCode || 'basic';
-const availablePlans = window.available_plans;
-const currentPlan = availablePlans[currentPlanId];
-const supportHost = useWindowProp('support_host');
+const currentRegion = computed(() => {
+  if (regionsEnabled && regions.value.current_jurisdiction) {
+    const defaultJurisdiction = regions.value.current_jurisdiction;
+    const jurisdiction: Jurisdiction | undefined = regions.value.jurisdictions.find(j => j.identifier === defaultJurisdiction) || regions.value.jurisdictions[0];
+    return jurisdiction;
+  } else {
+    throw new Error('No jurisdictions found or regions are not enabled.');
+  }
+});
 
+const socialProviders = [
+  { name: 'Google', icon: 'mdi:google' },
+  { name: 'GitHub', icon: 'mdi:github' },
+  { name: 'Twitter', icon: 'mdi:twitter' },
+];
 </script>
