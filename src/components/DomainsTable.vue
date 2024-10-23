eef253884442ceceba9704b6f8d619885b81ce06
delano <delano@onetimesecret.com>
2024-10-23 12:44:02 -0700

    [#20241018] Give the domains page a minimum height

--- src/components/DomainsTable.vue
+++ src/components/DomainsTable.vue
@@ -19,7 +19,7 @@
         </router-link>
       </div>
     </div>
-    <div class="mt-8 flow-root">
+    <div class="mt-8 flow-root min-h-72">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
