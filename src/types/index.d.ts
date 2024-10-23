551a17d0768549874cbc5fffb86532a2e9ddb86d
delano <delano@onetimesecret.com>
2024-10-22 21:33:58 -0700

    [#20241022] Type support for markdown components

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,4 @@ import './window.d.ts';
 import './vite-env.d.ts';
 import './locales.d.ts';
 import './onetime.d.ts';
+import './shims-md.d.ts';
---
+++ src/types/shims-md.d.ts
@@ -0,0 +1,5 @@
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
+}
