684fa3f247f3f7b5bcb1459884d3a4b7492f2edd
delano <delano@onetimesecret.com>
2024-10-24 00:44:03 -0700

    [#2024-10] Update footer layout for consistency
    Always on two lines.

--- src/components/layout/DefaultFooter.vue
+++ src/components/layout/DefaultFooter.vue
@@ -9,27 +9,13 @@
       <FooterLinkLists v-if="displayLinks"
                        v-bind="$props" />
 
-      <div class="
-        flex flex-col-reverse
-        justify-between items-center
-        pt-6 mt-6
-        space-y-6 space-y-reverse md:space-y-0
-        md:flex-row">
-        <div v-if="displayVersion"
-             class="
-          w-full md:w-auto
-          text-sm text-center md:text-left
-          text-gray-600 dark:text-gray-300">
-          &copy; {{ new Date().getFullYear() }} {{ companyName }}.
-          <span class="hidden md:inline">All rights reserved.</span>
-        </div>
-
-        <div v-if="displayToggles"
-             class="
-          flex flex-wrap
-          items-center justify-center md:justify-end
-          w-full md:w-auto
-          space-x-4">
+      <div class="flex flex-col space-y-6 pt-6 mt-6">
+        <div class="
+          flex
+          items-center justify-between
+          w-full
+          max-w-[400px]
+          mx-auto">
           <JurisdictionFooterNotice v-if="regionsEnabled && regions" />
 
           <ThemeToggle class="
@@ -45,17 +31,22 @@
             transition-colors duration-200"
                           aria-label="Provide feedback" />
         </div>
+
+        <div v-if="displayVersion"
+             class="
+          w-full
+          text-sm text-center
+          text-gray-600 dark:text-gray-300">
+          &copy; {{ new Date().getFullYear() }} {{ companyName }}.
+          All rights reserved.
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
-
-
-
-
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 import type { Props as DefaultProps } from '@/layouts/DefaultLayout.vue';
 import FeedbackToggle from '@/components/FeedbackToggle.vue';
 import ThemeToggle from '@/components/ThemeToggle.vue';
@@ -64,10 +55,10 @@ import JurisdictionFooterNotice from '@/components/JurisdictionFooterNotice.vue'
 import { useWindowProps } from '@/composables/useWindowProps';
 
 export interface Props extends DefaultProps {
-  displayFeedback?: boolean
-  displayLinks?: boolean
-  displayVersion?: boolean
-  displayToggles?: boolean
+  displayFeedback?: boolean;
+  displayLinks?: boolean;
+  displayVersion?: boolean;
+  displayToggles?: boolean;
 }
 
 withDefaults(defineProps<Props>(), {
@@ -77,6 +68,6 @@ withDefaults(defineProps<Props>(), {
   displayToggles: true,
 });
 
-const { regions_enabled: regionsEnabled, regions } = useWindowProps(['regions_enabled', 'regions'])
+const { regions_enabled: regionsEnabled, regions } = useWindowProps(['regions_enabled', 'regions']);
 const companyName = ref('Onetime Secret');
 </script>
