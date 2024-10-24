e96881b3df08751d25dc3dfe76b81d7e6a538296
delano <delano@onetimesecret.com>
2024-10-24 01:26:24 -0700

    [#2024-10] Match footer links styles with v0.18.2
    Slightly dimmed, underlined, larger, and in brand font.

--- src/components/layout/FooterLinkLists.vue
+++ src/components/layout/FooterLinkLists.vue
@@ -5,20 +5,20 @@
       <!-- Company links -->
       <div class="space-y-4">
         <h3 class="font-semibold text-gray-800 dark:text-gray-200 text-xl">Company</h3>
-        <ul class="space-y-2">
+        <ul class="prose dark:prose-invert font-brand space-y-2">
           <li>
             <router-link to="/about"
-                        class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+                        class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
                         aria-label="Learn about our company">About</router-link>
           </li>
           <li v-if="plansEnabled && authentication.enabled">
             <router-link to="/pricing"
-                        class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+                        class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
                         aria-label="View our subscription pricing">Pricing</router-link>
           </li>
           <li v-if="supportHost">
             <a :href="`${supportHost}/blog`"
-              class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+              class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
               aria-label="Read our latest blog posts"
               target="_blank"
               rel="noopener noreferrer">Blog</a>
@@ -29,10 +29,10 @@
       <!-- Resources links -->
       <div class="space-y-4">
         <h3 class="font-semibold text-gray-800 dark:text-gray-200 text-xl">Resources</h3>
-        <ul class="space-y-2">
+        <ul class="prose dark:prose-invert font-brand space-y-2">
           <li>
             <a href="https://github.com/onetimesecret/onetimesecret"
-              class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+              class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
               aria-label="View our source code on GitHub"
               target="_blank"
               rel="noopener noreferrer">GitHub</a>
@@ -40,20 +40,20 @@
           <li v-if="supportHost">
             <a :href="`${supportHost}/docs`"
               aria-label="Access our documentation"
-              class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+              class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
               target="_blank"
               rel="noopener noreferrer">Docs</a>
           </li>
           <li v-if="supportHost">
             <a :href="`${supportHost}/docs/rest-api`"
               aria-label="Explore our API documentation"
-              class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+              class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
               target="_blank"
               rel="noopener noreferrer">API</a>
           </li>
           <li>
             <a href="https://status.onetimesecret.com/"
-              class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+              class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
               aria-label="Check our service status"
               target="_blank"
               rel="noopener noreferrer">Status</a>
@@ -64,20 +64,20 @@
       <!-- Legal links -->
       <div class="space-y-4 col-span-2 md:col-span-1">
         <h3 class="font-semibold text-gray-800 dark:text-gray-200 text-xl">Legals</h3>
-        <ul class="space-y-2">
+        <ul class="prose dark:prose-invert font-brand space-y-2">
           <li>
             <router-link to="/info/privacy"
-                        class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+                        class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
                         aria-label="Read our Privacy Policy">Privacy</router-link>
           </li>
           <li>
             <router-link to="/info/terms"
-                        class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+                        class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
                         aria-label="View our Terms and Conditions">Terms</router-link>
           </li>
           <li>
             <router-link to="/info/security"
-                        class="text-gray-600 dark:text-gray-300 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-base"
+                        class="text-gray-600 dark:text-gray-400 hover:text-brand-500 dark:hover:text-brand-400 transition-colors duration-300 text-xl md:text-lg"
                         aria-label="Learn about our security measures">Security</router-link>
           </li>
         </ul>
