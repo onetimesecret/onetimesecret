b7afc3d9408751780dbf407e3a1989e2ff2a2dbe
delano <delano@onetimesecret.com>
2024-11-01 13:37:07 -0700

    [#816] Remove unused view helper methods

--- lib/onetime/app/web/views/view_helpers.rb
+++ lib/onetime/app/web/views/view_helpers.rb
@@ -4,11 +4,6 @@ module Onetime
   module App
     module Views
       module ViewHelpers # rubocop:disable Style/Documentation
-        include Onetime::Logic::LogicHelpers
-
-        def add_shrimp
-          format('<input type="hidden" name="shrimp" value="%s" />', sess.add_shrimp)
-        end
 
         def jsvar(name, value)
           value = case value.class.to_s
@@ -28,41 +23,6 @@ module Onetime
                     "console.error('#{value.class} is an unhandled type (named #{name})')"
                   end
           { name: name, value: value }
-        end
-
-        # Caches the result of a method call for a specified duration.
-        #
-        # This method is used to cache the result of expensive operations, such as
-        # asset generation, in Redis. It provides a simple way to implement caching
-        # for view helpers or other frequently called methods.
-        #
-        # @param methname [String] The name of the method being cached.
-        # @yield The block of code to execute if the cache is empty.
-        # @return [String] The cached content or the result of the block execution.
-        #
-        # @example
-        #   cached_method('generate_asset') do
-        #     # Expensive operation to generate an asset
-        #     Asset.generate_complex_asset
-        #   end
-        #
-        # @note The cache key is prefixed with "template:global:" and stored in Redis db 0.
-        # @note The default Time To Live (TTL) for the cache is 1 hour.
-        #
-        def cached_method methname
-          rediskey = "template:global:#{methname}"
-          cache_object = Familia::String.new rediskey, ttl: 1.hour, db: 0
-          OT.ld "[cached_method] #{methname} #{cache_object.exists? ? 'hit' : 'miss'} #{rediskey}"
-          cached = cache_object.get
-          return cached if cached
-
-          # Existing logic to generate assets...
-          content = yield
-
-          # Cache the generated content
-          cache_object.set(content)
-
-          content
         end
 
         def vite_assets
