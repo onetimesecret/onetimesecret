fe7d1435446b659194d302fb28d4af7ef49f3f32
delano <delano@onetimesecret.com>
2024-11-10 22:17:39 -0800

    [#838] Remove debug logging

--- lib/onetime/logic/secrets/burn_secret.rb
+++ lib/onetime/logic/secrets/burn_secret.rb
@@ -13,68 +13,34 @@ module Onetime::Logic
       end
 
       def raise_concerns
-        #limit_action :burn_secret
+        limit_action :burn_secret
         raise OT::MissingSecret if metadata.nil?
       end
 
-      require 'logger'
-
       def process
-        logger = Logger.new(STDOUT)
-        logger.level = Logger::DEBUG
-
-        logger.debug("Starting process method")
-
         potential_secret = @metadata.load_secret
-        logger.debug("Loaded potential secret: #{potential_secret.inspect}")
 
         if potential_secret
           @correct_passphrase = !potential_secret.has_passphrase? || potential_secret.passphrase?(passphrase)
-          logger.debug("Correct passphrase: #{@correct_passphrase}")
-
           viewable = potential_secret.viewable?
-          logger.debug("Secret viewable: #{viewable}")
-
           continue_result = params[:continue]
-          logger.debug("Continue result: #{continue_result} #{continue_result.class}")
-
           @greenlighted = viewable && correct_passphrase && continue_result
-          logger.debug("Greenlighted: #{@greenlighted}")
 
           if greenlighted
             @secret = potential_secret
-            logger.debug("Secret set: #{@secret.inspect}")
-
             owner = secret.load_customer
-            logger.debug("Loaded owner: #{owner.inspect}")
-
             secret.burned!
-            logger.debug("Secret burned")
-
             owner.increment_field :secrets_burned unless owner.anonymous?
-            logger.debug("Owner secrets burned incremented")
-
             OT::Customer.global.increment_field :secrets_burned
-            logger.debug("Global secrets burned incremented")
-
             OT::Logic.stathat_count('Burned Secrets', 1)
-            logger.debug("Stathat count incremented")
-
           elsif !correct_passphrase
-            logger.debug("Incorrect passphrase")
-
             limit_action :failed_passphrase if !potential_secret.has_passphrase?
-            logger.debug("Rate limit action taken")
-
             message = OT.locales.dig(locale, :web, :COMMON, :error_passphrase) || 'Incorrect passphrase'
-            logger.debug("Error message: #{message}")
-
             raise_form_error message
           end
         end
-
-        logger.debug("Process method completed")
       end
+
       def success_data
         {
           success: greenlighted,
