8e79e02762bffba1b364df521031130960f1c833
delano <delano@onetimesecret.com>
2024-11-18 18:30:23 -0800

    [#861] Correction for familia v1.1 method name
    Updates Redis key deletion method from hdel to del
    Corrects method usage for compatibility with Familia v1.1 library

--- lib/onetime/models/custom_domain.rb
+++ lib/onetime/models/custom_domain.rb
@@ -207,7 +207,7 @@ class Onetime::CustomDomain < Familia::Horreum
 
     redis.multi do |multi|
       # Delete all associated keys
-      keys_to_delete.each { |key| multi.hdel(rediskey, key) }
+      keys_to_delete.each { |key| multi.del(rediskey, key) }
 
       # Remove from global values set
       multi.zrem(self.class.values.rediskey, identifier)
