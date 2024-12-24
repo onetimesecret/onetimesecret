28bca1a532217a5e66c7ea839293f63e7fa4e469
delano <delano@onetimesecret.com>
2024-12-23 17:24:51 -0800

    [#906] Cleanup error display template
    Sort of a replacement for BasicAlerts, but very much a wip.

--- src/components/ErrorDisplay.vue
+++ src/components/ErrorDisplay.vue
@@ -15,26 +15,22 @@ const friendlyMessage = computed(() => {
 </script>
 
 <template>
-  <div class="my-4 rounded-md bg-red-50 p-4">
-    <div class="flex">
-      <div class="shrink-0">
-        <svg
-          class="size-5 text-red-400"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor">
-          <path
-            fill-rule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-      <div class="ml-3">
-        <h3 class="text-sm font-medium text-red-800">
-          {{ friendlyMessage }}
-        </h3>
-      </div>
+  <div class="rounded-md bg-red-50 p-4 dark:bg-red-900/20">
+    <div class="flex items-start">
+      <svg
+        class="size-5 text-red-400 dark:text-red-300"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor">
+        <path
+          fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <p class="ml-3 text-sm font-medium text-red-800 dark:text-red-200">
+        {{ friendlyMessage }}
+      </p>
     </div>
   </div>
 </template>
