a0e7ae30661640a88ffd1cd731897f255ccc4324
delano <delano@onetimesecret.com>
2024-12-29 13:08:08 -0800

    [#925] Just warn in tests (no-unused-expressions)
    @typescript-eslint/no-unused-expressions

--- eslint.config.ts
+++ eslint.config.ts
@@ -484,7 +484,7 @@ export default [
       // Note: you must disable the base rule as it can report incorrect errors
       'no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-expressions': [
-        'error',
+        'warn',
         {
           allowShortCircuit: true,
           allowTernary: true,
