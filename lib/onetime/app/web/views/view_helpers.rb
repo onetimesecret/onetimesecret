2819e531197efc7e65cf3a8bff7d33593e6c07a1
delano <delano@onetimesecret.com>
2025-01-07 21:43:41 -0800

    [#878] Add nonce to all vite assets in prod

--- lib/onetime/app/web/views/view_helpers.rb
+++ lib/onetime/app/web/views/view_helpers.rb
@@ -49,7 +49,7 @@ module Onetime
         end
 
         def vite_assets(nonce: nil)
-          nonce ||= self[:nonce]
+          nonce ||= self[:nonce] # we allow overriding the nonce for testing
 
           manifest_path = File.join(PUBLIC_DIR, 'dist', '.vite', 'manifest.json')
           unless File.exist?(manifest_path)
@@ -66,18 +66,18 @@ module Onetime
 
           if main_entry
             # Add the main JavaScript file
-            assets << %(<script type="module" src="/dist/#{main_entry['file']}"></script>)
+            assets << %(<script type="module" nonce="#{nonce}" src="/dist/#{main_entry['file']}"></script>)
 
             # Add the main CSS file if it exists
             if main_entry['css'] && main_entry['css'].first
-              assets << %(    <link rel="stylesheet" href="/dist/#{main_entry['css'].first}">)
+              assets << %(    <link rel="stylesheet" nonce="#{nonce}" href="/dist/#{main_entry['css'].first}">)
             end
 
             # Add preloads for font files
             font_files = @manifest_cache.values.select { |v| v['file'] =~ /\.(woff2?|ttf|otf|eot)$/ }
             font_files.each do |font|
               file_extension = File.extname(font['file']).delete('.')
-              assets << %(    <link rel="preload" href="/dist/#{font['file']}" as="font" type="font/#{file_extension}" crossorigin>)
+              assets << %(    <link rel="preload" nonce="#{nonce}" href="/dist/#{font['file']}" as="font" type="font/#{file_extension}" crossorigin>)
             end
           else
             OT.le "Main entry point not found in Vite manifest at #{manifest_path}"
