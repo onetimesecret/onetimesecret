829b84f745542448cfd29f608da7b3c94a08929c
delano <delano@onetimesecret.com>
2025-01-11 11:22:33 -0800

    [#952] Add OIcon
    * Introduces new OIcon component
    * Switches from @iconify/vue to internal SVG-based implementation

---
+++ src/components/icons/OIcon.vue
@@ -0,0 +1,22 @@
+<!--
+  Usage:
+
+    <OIcon
+      class="size-5"
+      collection="heroicons-solid"
+      name="ellipsis-vertical" />
+
+-->
+<script setup lang="ts">
+  export interface Props {
+    collection: string; // heroicons-solid
+    name: string; // ellipses-vertical
+  }
+  defineProps<Props>();
+</script>
+
+<template>
+  <svg aria-hidden="true">
+    <use :href="`#${collection}-${name}`" />
+  </svg>
+</template>
