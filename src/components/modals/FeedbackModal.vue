3f40a24e0caccaf32431bd5d6e55769767530093
delano <delano@onetimesecret.com>
2025-01-11 10:38:36 -0800

    [#952] Fix linking to adjacent spaces

--- src/components/modals/FeedbackModal.vue
+++ src/components/modals/FeedbackModal.vue
@@ -34,41 +34,34 @@ onBeforeUnmount(() => {
 
 <template>
   <teleport to="body">
-    <div
-      v-if="isOpen"
-      class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4 dark:bg-black/70 sm:p-6"
-      aria-labelledby="feedback-modal-title"
-      role="dialog"
-      aria-modal="true">
-      <FocusTrap
-        :active="isOpen"
-        :initial-focus="() => $refs.closeButton"
-        :click-outside-deactivates="true"
-        :escape-deactivates="true"
-        @deactivate="close">
+    <div v-if="isOpen"
+         class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4 dark:bg-black/70 sm:p-6"
+         aria-labelledby="feedback-modal-title"
+         role="dialog"
+         aria-modal="true">
+      <FocusTrap :active="isOpen"
+                 :initial-focus="() => $refs.closeButton"
+                 :click-outside-deactivates="true"
+                 :escape-deactivates="true"
+                 @deactivate="close">
         <div class="max-h-[90vh] w-full max-w-md overflow-y-auto rounded-lg bg-white p-6 shadow-xl dark:bg-gray-800">
           <div class="mb-6 flex items-center justify-between">
-            <h2
-              id="feedback-modal-title"
-              class="text-2xl font-semibold text-gray-900 dark:text-white">
+            <h2 id="feedback-modal-title"
+                class="text-2xl font-semibold text-gray-900 dark:text-white">
               Share your feedback
             </h2>
-            <button
-              ref="closeButton"
-              @click="close"
-              class="text-gray-400 transition-colors hover:text-gray-500 dark:hover:text-white"
-              aria-label="Close feedback modal">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="size-6"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true">
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
+            <button ref="closeButton"
+                    @click="close"
+                    class="text-gray-400 transition-colors hover:text-gray-500 dark:hover:text-white"
+                    aria-label="Close feedback modal">
+              <svg xmlns="http://www.w3.org/2000/svg"
+                   class="size-6"
+                   viewBox="0 0 20 20"
+                   fill="currentColor"
+                   aria-hidden="true">
+                <path fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd" />
               </svg>
             </button>
           </div>
@@ -76,11 +69,8 @@ onBeforeUnmount(() => {
           <FeedbackModalForm :show-red-button="true" />
 
           <p class="mt-6 text-center text-sm italic text-gray-500 dark:text-gray-400">
-            Your feedback <RouterLink
-              to="/feedback"
-              class="underline">
-              helps us improve
-            </RouterLink>. Especially the annoyances!
+            Your feedback <RouterLink to="/feedback"
+                        class="underline">helps us improve</RouterLink>. Especially the annoyances!
           </p>
         </div>
       </FocusTrap>
