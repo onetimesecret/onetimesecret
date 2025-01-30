e8cc236b60ac7208e21232178f76940bfda48be7
delano <delano@onetimesecret.com>
2025-01-29 23:18:46 -0800

    [#20250129] Add region badge to masthead

--- src/components/layout/MastHead.vue
+++ src/components/layout/MastHead.vue
@@ -6,6 +6,8 @@ import { WindowService } from '@/services/window.service';
 import type { LayoutProps } from '@/types/ui/layouts';
 import OIcon from '@/components/icons/OIcon.vue';
 import { computed, ref } from 'vue';
+import { useJurisdictionStore } from '@/stores/jurisdictionStore';
+import type { Jurisdiction } from '@/schemas/models/jurisdiction';
 
 withDefaults(defineProps<LayoutProps>(), {
   displayMasthead: true,
@@ -31,27 +33,38 @@ const closeSettingsModal = () => {
   isSettingsModalOpen.value = false;
 };
 
+const jurisdictionStore = useJurisdictionStore();
+const currentJurisdiction = computed<Jurisdiction | null>(
+  () => jurisdictionStore.getCurrentJurisdiction
+);
 </script>
 
 <template>
 
-<div v-if="displayMasthead"
-     class="w-full">
-  <div class="flex flex-col items-center justify-between sm:flex-row">
-    <div class="mb-4 sm:mb-0">
-      <router-link to="/"
-                   class="flex items-center">
-        <img id="logo"
-             src="@/assets/img/onetime-logo-v3-xl.svg"
-             class="size-12 rounded-md sm:size-16"
-             height="64"
-             alt="Onetime Secret" />
-        <span class="ml-2 font-brand text-xl font-bold text-gray-800 dark:text-white">
-          Onetime Secret
-        </span>
-      </router-link>
-    </div>
-
+  <div v-if="displayMasthead" class="w-full">
+    <div class="flex flex-col items-center justify-between sm:flex-row">
+      <div class="mb-4 sm:mb-0">
+        <div class="flex flex-col">
+          <router-link to="/" class="flex items-center">
+            <img id="logo"
+                 src="@/assets/img/onetime-logo-v3-xl.svg"
+                 class="size-12 rounded-md sm:size-16"
+                 height="64"
+                 alt="Onetime Secret" />
+            <div class="ml-2 flex flex-col">
+              <span class="font-brand text-xl font-bold text-gray-800 dark:text-white">
+                Onetime Secret
+              </span>
+              <div v-if="currentJurisdiction"
+                   class="mt-0.5 w-fit rounded-full
+                          bg-gray-100 px-1.5 py-0.5 text-xs font-medium
+                          text-gray-600 dark:bg-gray-800 dark:text-gray-400">
+                {{ currentJurisdiction.identifier }}
+              </div>
+            </div>
+          </router-link>
+        </div>
+      </div>
     <nav v-if="displayNavigation"
          role="navigation"
          aria-label="Main navigation"
