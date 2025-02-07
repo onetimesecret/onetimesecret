fd8d9822addbd33c5763bd434b3bc597f282f36f
delano <delano@onetimesecret.com>
2025-02-07 13:22:28 -0800

    [#I18] Use OIcon in jurisdiction toggle

--- src/components/JurisdictionFooterNotice.vue
+++ src/components/JurisdictionFooterNotice.vue
@@ -127,15 +127,10 @@ const getIconName = (jurisdiction: Jurisdiction | null): string => {
             <span v-if="currentJurisdiction?.identifier === jurisdiction.identifier"
                   class="absolute inset-y-0 right-0 flex items-center pr-4 text-brand-600
                      dark:text-brand-400">
-              <svg class="size-5"
-                   xmlns="http://www.w3.org/2000/svg"
-                   viewBox="0 0 20 20"
-                   fill="currentColor"
-                   aria-hidden="true">
-                <path fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd" />
-              </svg>
+              <OIcon
+                collection="heroicons"
+                name="check-20-solid"
+                class="size-5 text-brand-600 dark:text-brand-400" />
             </span>
           </a>
         </li>
