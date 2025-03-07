c1895d566b02726a5c72a1d6b9a0e599f42fd162
delano <delano@onetimesecret.com>
2025-03-06 21:33:34 -0800

    [#1142] Expand test cases for i18n config

--- tests/unit/ruby/rspec/config/i18n_spec.rb
+++ tests/unit/ruby/rspec/config/i18n_spec.rb
@@ -43,11 +43,112 @@ RSpec.describe "Internationalization config" do
       end
 
       context 'when internationalization is disabled' do
+        let(:original_state) do
+           {
+             i18n_enabled: described_class.i18n_enabled,
+             locales: described_class.instance_variable_get(:@locales),
+             default_locale: described_class.default_locale,
+             supported_locales: described_class.supported_locales
+           }
+         end
 
+         before do
+           # Setup disabled internationalization state
+           described_class.i18n_enabled = false
+           described_class.default_locale = 'en'
+           described_class.supported_locales = ['en']
+
+           # Simulate loading only English locale
+           en_locale = {'greeting': 'Hello'}
+           described_class.instance_variable_set(:@locales, {'en' => en_locale})
+         end
+
+         after do
+           # Restore original state
+           described_class.i18n_enabled = original_state[:i18n_enabled]
+           described_class.instance_variable_set(:@locales, original_state[:locales])
+           described_class.default_locale = original_state[:default_locale]
+           described_class.supported_locales = original_state[:supported_locales]
+         end
+
+         it 'returns only English locale' do
+           expect(described_class.locales).to include('en')
+           expect(described_class.locales.keys.length).to eq(1)
+         end
+
+         it 'has English as default locale' do
+           expect(described_class.default_locale).to eq('en')
+         end
+
+         it 'only has English in supported locales' do
+           expect(described_class.supported_locales).to eq(['en'])
+         end
+
+         it 'accesses locale content correctly' do
+           expect(described_class.locales['en'][:greeting]).to eq('Hello')
+         end
+      end
+
+      context 'when internationalization is enabled' do
+        let(:original_state) do
+          {
+            i18n_enabled: described_class.i18n_enabled,
+            locales: described_class.instance_variable_get(:@locales),
+            default_locale: described_class.default_locale,
+            supported_locales: described_class.supported_locales,
+            fallback_locale: described_class.fallback_locale
+          }
+        end
+
+        before do
+          # Setup enabled internationalization state
+          described_class.i18n_enabled = true
+          described_class.default_locale = 'fr_FR'
+          described_class.supported_locales = ['en', 'fr_FR', 'de_AT']
+          described_class.fallback_locale = {'fr-CA': ['fr_CA', 'fr_FR', 'en'], 'default': ['en']}
+
+          # Simulate loading multiple locales
+          test_locales = {
+            'en' => {greeting: 'Hello'},
+            'fr_FR' => {greeting: 'Bonjour'},
+            'de_AT' => {greeting: 'Grüß Gott'}
+          }
+          described_class.instance_variable_set(:@locales, test_locales)
+        end
+
+        after do
+          # Restore original state
+          described_class.i18n_enabled = original_state[:i18n_enabled]
+          described_class.instance_variable_set(:@locales, original_state[:locales])
+          described_class.default_locale = original_state[:default_locale]
+          described_class.supported_locales = original_state[:supported_locales]
+          described_class.fallback_locale = original_state[:fallback_locale]
+        end
+
+        it 'returns all configured locales' do
+          expect(described_class.locales.keys).to contain_exactly('en', 'fr_FR', 'de_AT')
+        end
+
+        it 'uses French as default locale' do
+          expect(described_class.default_locale).to eq('fr_FR')
+        end
+
+        it 'includes all locales in supported locales' do
+          expect(described_class.supported_locales).to eq(['en', 'fr_FR', 'de_AT'])
+        end
+
+        it 'accesses specific locale content correctly' do
+          expect(described_class.locales['fr_FR'][:greeting]).to eq('Bonjour')
+          expect(described_class.locales['de_AT'][:greeting]).to eq('Grüß Gott')
+        end
+
+        it 'respects configured fallback locales' do
+          expect(described_class.fallback_locale[:'fr-CA']).to eq(['fr_CA', 'fr_FR', 'en'])
+          expect(described_class.fallback_locale[:default]).to eq(['en'])
+        end
       end
 
     end
-
   end
 
   describe Onetime::App::WebHelpers do
