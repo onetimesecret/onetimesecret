2f1cd7d2f363c76fa1b3da54f53d99769037dc0e
delano <delano@onetimesecret.com>
2025-03-30 20:03:11 -0700

    [#1189] Remove unnecessary inclusion of Core::Views::SanitizerHelpers in ViewHelpers module
    Also resolves load error in cli mode:
    
    LoadError: cannot load such file -- core/views/helpers/sanitizer

--- lib/onetime/mail/views/view_helpers.rb
+++ lib/onetime/mail/views/view_helpers.rb
@@ -1,12 +1,10 @@
 # lib/onetime/mail/views/view_helpers.rb
 
-require 'core/views/helpers/sanitizer'
 
 module Onetime
   module Mail
 
     module ViewHelpers # rubocop:disable Style/Documentation
-      include Core::Views::SanitizerHelpers
     end
 
   end
