9fb3c62c8d876f149ec906d2dbfdc3d63f88b4c6
delano <delano@onetimesecret.com>
2025-04-15 23:26:37 -0700

    [#1251] Formatting

--- src/views/dashboard/DashboardRecent.vue
+++ src/views/dashboard/DashboardRecent.vue
@@ -1,70 +1,67 @@
 <script setup lang="ts">
-import DashboardTabNav from '@/components/dashboard/DashboardTabNav.vue';
-import EmptyState from '@/components/EmptyState.vue';
-import ErrorDisplay from '@/components/ErrorDisplay.vue';
-import SecretMetadataTable from '@/components/secrets/SecretMetadataTable.vue';
-import { MetadataRecords } from '@/schemas/api/endpoints';
-import { useMetadataList } from '@/composables/useMetadataList';
-import { onMounted, computed } from 'vue';
-import TableSkeleton from '@/components/closet/TableSkeleton.vue';
+  import TableSkeleton from '@/components/closet/TableSkeleton.vue';
+  import DashboardTabNav from '@/components/dashboard/DashboardTabNav.vue';
+  import EmptyState from '@/components/EmptyState.vue';
+  import ErrorDisplay from '@/components/ErrorDisplay.vue';
+  import SecretMetadataTable from '@/components/secrets/SecretMetadataTable.vue';
+  import { useMetadataList } from '@/composables/useMetadataList';
+  import { MetadataRecords } from '@/schemas/api/endpoints';
+  import { onMounted, computed } from 'vue';
 
-// Define props
-interface Props {
-}
-defineProps<Props>();
+  // Define props
+  interface Props {}
+  defineProps<Props>();
 
-const { details, recordCount, isLoading, refreshRecords, error } = useMetadataList();
+  const { details, recordCount, isLoading, refreshRecords, error } = useMetadataList();
 
-// Add computed properties for received and not received items
-const receivedItems = computed(() => {
-  if (details.value) {
-    return details.value.received;
-  }
-  return [] as MetadataRecords[];
-});
+  // Add computed properties for received and not received items
+  const receivedItems = computed(() => {
+    if (details.value) {
+      return details.value.received;
+    }
+    return [] as MetadataRecords[];
+  });
 
-const notReceivedItems = computed(() => {
-  if (details.value) {
-    return details.value.notreceived;
-  }
-  return [] as MetadataRecords[];
-});
+  const notReceivedItems = computed(() => {
+    if (details.value) {
+      return details.value.notreceived;
+    }
+    return [] as MetadataRecords[];
+  });
 
-onMounted(() => {
-  refreshRecords()
-});
-
+  onMounted(() => {
+    refreshRecords();
+  });
 </script>
 
 <template>
   <div>
     <DashboardTabNav />
 
-    <ErrorDisplay v-if="error" :error="error" />
+    <ErrorDisplay
+      v-if="error"
+      :error="error" />
 
     <div v-else-if="isLoading">
-      <TableSkeleton/>
+      <TableSkeleton />
     </div>
 
     <div v-else>
-
       <SecretMetadataTable
         v-if="recordCount > 0"
         :not-received="notReceivedItems"
         :received="receivedItems"
-        :is-loading="isLoading"
-      />
+        :is-loading="isLoading" />
       <EmptyState
         v-else
         actionRoute="/"
         :actionText="$t('create-a-secret')">
         <template #title>
-        {{ $t('web.dashboard.title_no_recent_secrets') }}
+          {{ $t('web.dashboard.title_no_recent_secrets') }}
         </template>
         <template #description>
-        <div>{{ $t('web.dashboard.get-started-by-creating-your-first-secret') }}</div>
-        <div>{{ $t('theyll-appear-here-once-youve-shared-them') }}</div>
-
+          <div>{{ $t('web.dashboard.get-started-by-creating-your-first-secret') }}</div>
+          <div>{{ $t('theyll-appear-here-once-youve-shared-them') }}</div>
         </template>
       </EmptyState>
     </div>
