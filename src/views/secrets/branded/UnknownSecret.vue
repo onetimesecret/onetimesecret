45e1844be3eff4488f307f3a0da0d57cfcca84a7
delano <delano@onetimesecret.com>
2025-04-18 17:29:41 -0700

    Fix branded button text color

--- src/views/secrets/branded/UnknownSecret.vue
+++ src/views/secrets/branded/UnknownSecret.vue
@@ -69,17 +69,10 @@
       <router-link
         to="/"
         class="inline-block rounded-lg border-2 bg-white px-4 py-2 transition duration-300 ease-in-out hover:bg-brand-100 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-brand-400 dark:focus:ring-offset-gray-900"
-        :style="
-          brandSettings?.primary_color
-            ? {
-                color: brandSettings.primary_color,
-                borderColor: brandSettings.primary_color,
-              }
-            : {
-                color: 'var(--color-brand-500)',
-                borderColor: 'var(--color-brand-500)',
-              }
-        ">
+        :style="{
+          backgroundColor: brandSettings?.primary_color ?? '#dc4a22',
+          color: brandSettings?.button_text_light ?? true ? '#ffffff' : '#222222',
+        }">
         {{ $t('return-to-home') }}
       </router-link>
     </template>
