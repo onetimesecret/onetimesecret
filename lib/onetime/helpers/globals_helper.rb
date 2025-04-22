7aa300a1304a23b25dd5a40f6bc680013ee17052
delano <delano@onetimesecret.com>
2025-04-21 22:55:21 -0700

    [#1271] Change default secret fallback from 'CHANGEME' to nil
    Avoids accidentally running the application with the global secret set
    to CHANGEME. Instead the fast-fail when nil logic will handle the
    situation.

--- lib/onetime/helpers/globals_helper.rb
+++ lib/onetime/helpers/globals_helper.rb
@@ -5,7 +5,7 @@ module Onetime
     attr_reader :global_secret, :global_banner
 
     def set_global_secret
-      @global_secret = OT.conf[:site][:secret] || 'CHANGEME'
+      @global_secret = OT.conf[:site][:secret] || nil
       unless Gibbler.secret && Gibbler.secret.frozen?
         Gibbler.secret = global_secret.freeze
       end
