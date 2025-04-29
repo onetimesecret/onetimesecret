f465a02d558ca7f35785fc66ad65e95a7c53c772
delano <delano@onetimesecret.com>
2025-04-28 20:57:15 -0700

    Add missing /feedback form route
    Also fixes timezone string

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -188,7 +188,7 @@ const {
                 d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            {{ $t('web.account.timezone', [userTimezone]) }}
+            {{ $t('web.account.timezone') }}: {{ userTimezone }}
           </li>
           <li class="flex items-center">
             <svg
