295590a52d251ac3be770892fc2950c4c37b13da
delano <delano@onetimesecret.com>
2025-05-06 12:57:46 -0700

    Add optional tooltip override

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -2,23 +2,26 @@
 
 <script setup lang="ts">
   import OIcon from '@/components/icons/OIcon.vue';
-  import { ref, onBeforeUnmount } from 'vue';
+  import { ref, computed, onBeforeUnmount } from 'vue';
   import { useI18n } from 'vue-i18n';
   const { t } = useI18n();
 
   interface Props {
     text: string;
     interval?: number;
+    tooltip?: string | undefined;
   }
 
   const props = withDefaults(defineProps<Props>(), {
     text: '',
     interval: 2000,
+    tooltip: undefined,
   });
 
   const copied = ref(false);
   const showTooltip = ref(false);
   const ariaLabel = copied.value ? t('web.STATUS.copied') : t('web.LABELS.copy_to_clipboard');
+  const tooltipText = computed(() => props.tooltip ? props.tooltip : ariaLabel);
 
   let tooltipTimeout: number | null = null;
 
@@ -57,18 +60,18 @@
     <!-- prettier-ignore-attribute class -->
     <div
       v-if="showTooltip"
-      class="absolute right-4 top-4 flex items-center gap-2 rounded-md
+      class="absolute bottom-1/2 right-full mr-2 z-50 flex items-center gap-2 rounded-md
         bg-slate-900 px-3.5 py-2 text-sm text-white shadow-lg transition-all duration-300
         dark:bg-slate-700"
       :class="{
-        'translate-y-1 opacity-0': !showTooltip,
-        'translate-y-0 opacity-100': showTooltip,
+        'translate-x-1 opacity-0': !showTooltip,
+        'translate-x-0 opacity-100': showTooltip,
       }">
       <OIcon
         collection="material-symbols"
         name="check-circle-outline"
         class="size-4 text-green-400" />
-      {{ ariaLabel }}
+      {{ tooltipText }}
     </div>
   </div>
 </template>
