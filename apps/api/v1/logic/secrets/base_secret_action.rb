103897831d180621a22b92289703b31ca5e976da
delano <delano@onetimesecret.com>
2025-05-20 21:41:00 -0700

    Make sure we have failover ttl defaults
    Without these we can't set the min and max values to establish the time
    range. In the event that an install somehow doesn't have settings for
    this, insofar as the application runs and they're okay with the limited
    options made available by these values then, why raise a fuss.

--- apps/api/v1/logic/secrets/base_secret_action.rb
+++ apps/api/v1/logic/secrets/base_secret_action.rb
@@ -77,7 +77,10 @@ module V1::Logic
 
         # Get configuration options. We can rely on these values existing
         # because that are guaranteed by OT::Config.after_load.
-        secret_options = OT.conf[:site][:secret_options]
+        secret_options = OT.conf[:site].fetch(:secret_options, {
+          default_ttl: 7.days,
+          ttl_options: [1.minute, 1.hour, 1.day, 7.days]
+        })
         default_ttl = secret_options[:default_ttl]
         ttl_options = secret_options[:ttl_options]
 
