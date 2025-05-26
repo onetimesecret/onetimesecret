ae0e550f7d97a90c88e6a271203eb9af8f492451
delano <delano@onetimesecret.com>
2025-05-25 21:01:57 -0700

    Tidy up V2::Logic modules

--- apps/api/v2/logic/account.rb
+++ apps/api/v2/logic/account.rb
@@ -1,6 +1,14 @@
 # apps/api/v2/logic/account.rb
 
 require_relative 'base'
+
+module V2
+  module Logic
+    module Account
+    end
+  end
+end
+
 require_relative 'account/create_account'
 require_relative 'account/destroy_account'
 require_relative 'account/generate_api_token'
