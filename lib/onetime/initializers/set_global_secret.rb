90663550f70951d3002ced817b85d68b8fcb59f7
delano <delano@onetimesecret.com>
2025-06-05 00:01:38 -0700

    [#1452] Convert initializers to modules
    Move initialization logic for connecting databases, loading locales,
    and setting the global secret into `self.run` class methods within
    their respective nested modules.
    
    This improves modularity and organization of the initialization
    process.

--- lib/onetime/initializers/set_global_secret.rb
+++ lib/onetime/initializers/set_global_secret.rb
@@ -4,15 +4,36 @@ require 'onetime/refinements/hash_refinements'
 
 module Onetime
   module Initializers
-    attr_reader :global_secret
+    module SetGlobalSecret
 
-    using IndifferentHashAccess
+      using IndifferentHashAccess
 
-    def set_global_secret
-      @global_secret = OT.conf[:site][:secret] || nil
-      unless Gibbler.secret && Gibbler.secret.frozen?
-        Gibbler.secret = global_secret.freeze
+      def self.run(options = {})
+        # Logic from the original Onetime.set_global_secret method
+        # Access configuration via OT.conf
+        # Example:
+        if OT.conf[:site].nil? || OT.conf[:site][:secret].nil?
+          raise OT::Problem, "Primary secret is not configured (site.secret)"
+        end
+        OT.global_secret = OT.conf[:site][:secret]
+        OT.ld "[initializer] Global secret set"
       end
+
     end
   end
 end
+
+# module Onetime
+#   module Initializers
+#     attr_reader :global_secret
+#
+#     using IndifferentHashAccess
+#
+#     def set_global_secret
+#       @global_secret = OT.conf[:site][:secret] || nil
+#       unless Gibbler.secret && Gibbler.secret.frozen?
+#         Gibbler.secret = global_secret.freeze
+#       end
+#     end
+#   end
+# end
