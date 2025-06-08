d4327813f5a185d4798f233781f3a33765864e9e
delano <delano@onetimesecret.com>
2025-06-08 00:40:04 -0700

    [#1459] Remove unused methods

--- lib/onetime/config/load.rb
+++ lib/onetime/config/load.rb
@@ -4,24 +4,6 @@ module Onetime
   class Config
     module Load
       extend self
-      def load_custom_schema(schema_path)
-      rescue JSON::ParserError => e
-        raise OT::ConfigError, "Invalid JSON schema: #{e.message}"
-      end
-
-      def load_static_configuration(path=nil)
-      rescue OT::ConfigError => e
-        if OT.debug? && @parsed_template
-          template_lines = @parsed_template.result.split("\n")
-          template_lines.each_with_index do |line, index|
-            OT.ld "Line #{index + 1}: #{line}"
-          end
-        end
-
-        OT.le e.message
-        OT.ld e.backtrace.join("\n")
-        raise e
-      end
 
       def json_load_file(path)
         json = File.read(path)
