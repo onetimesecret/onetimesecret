1700a51ab0f9ac373ef3f7c5cf8d3e16cb6daf32
delano <delano@onetimesecret.com>
2025-07-03 11:49:57 -0700

    [#1529] Fix for writing to $stdout
    And not STDOUT. Using the global constant bypasses attempts to capture
    the output (e.g. in tests).

--- tests/unit/ruby/try/test_helpers.rb
+++ tests/unit/ruby/try/test_helpers.rb
@@ -1,22 +1,5 @@
 # tests/unit/ruby/try/test_helpers.rb
 
-# A custom hash class that allows indifferent access to keys.
-# Keys can be accessed using strings, symbols, or methods.
-# Helpul
-#
-#
-# Example:
-#
-#     http_response = IndifferentHash.new(
-#       code: 403,
-#       error: 'Forbidden',
-#       body: 'You are not allowed to access this resource.'
-#     )
-#
-#     http_response.code      # => 403
-#     http_response[:error]   # => 'Forbidden'
-#     http_response['body']   # => 'You are not allowed to access this resource.'
-#
 # Establish the environment
 ENV['RACK_ENV'] ||= 'production'
 ENV['ONETIME_HOME'] ||= File.expand_path('../../../../..', __FILE__).freeze
