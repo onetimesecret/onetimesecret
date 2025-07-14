f23a991caaf9a4da6b702181194d815f6abb39bd
delano <delano@onetimesecret.com>
2025-07-14 10:18:44 -0700

    Reorganize class settings tryouts

--- tryouts/controllers/class_settings_try.rb
+++ tryouts/controllers/class_settings_try.rb
@@ -1,20 +1,21 @@
 # tryouts/config/app_settings_try.rb
 
-# These tryouts test the functionality of the AppSettings module in the Manifold::Views::BaseView class.
-# The AppSettings module provides configuration options for UTF-8 and URI encoding
+# These tryouts test the functionality ofthe V2::Controllers::ClassSettings class.
+#
+# The ClassSettings module provides configuration options for UTF-8 and URI encoding
 # middleware checks.
 #
-# We're testing various aspects of the AppSettings module, including:
+# We're testing various aspects of the ClassSettings module, including:
 # 1. Default values for check_utf8 and check_uri_encoding
 # 2. Setting and getting check_utf8 and check_uri_encoding values
 # 3. Independence of settings between different classes
 # 4. Behavior when including the module in multiple classes
 #
-# These tests aim to ensure that the AppSettings module correctly manages
+# These tests aim to ensure that the ClassSettings module correctly manages
 # configuration options for UTF-8 and URI encoding checks, which is crucial
 # for properly handling incoming requests in the Onetime application.
 #
-# The tryouts simulate different scenarios of using the AppSettings module
+# The tryouts simulate different scenarios of using the ClassSettings module
 # without needing to run the full application, allowing for targeted testing
 # of this specific functionality.
 
