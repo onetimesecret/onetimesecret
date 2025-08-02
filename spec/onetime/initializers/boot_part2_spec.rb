30686eb796bc0c2e21007c6a0013ba785242e695
delano <delano@onetimesecret.com>
2025-08-01 20:11:29 -0700

    [#1554] SystemSettings#extract_colonel_config was removed

--- spec/onetime/initializers/boot_part2_spec.rb
+++ spec/onetime/initializers/boot_part2_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe "Onetime global state after boot" do
 
     # Mock system settings setup methods
     allow(V2::SystemSettings).to receive(:current).and_raise(OT::RecordNotFound.new("No config found"))
-    allow(V2::SystemSettings).to receive(:extract_colonel_config).and_return({})
+    # allow(V2::SystemSettings).to receive(:extract_colonel_config).and_return({})
     allow(V2::SystemSettings).to receive(:create).and_return(double('SystemSettings', rediskey: 'test:config'))
 
     # Other common mocks
