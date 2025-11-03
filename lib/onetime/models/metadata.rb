e9db8d5b33499bc7d12f1a83fae7cc583ce336ab
delano <delano@onetimesecret.com>
2025-11-02 22:10:41 -0800

    Refactor Metadata model field definitions into features
    Move legacy fields, safe_dump configuration, and state transition
    methods into separate feature modules. Update field names for
    consistency (custid→owner_id, key→identifier,
    secret_key→secret_identifier) and add spawn_pair method to class
    methods.

--- lib/onetime/models/metadata.rb
+++ lib/onetime/models/metadata.rb
@@ -6,27 +6,20 @@ module Onetime
 
     using Familia::Refinements::TimeLiterals
 
-    feature :safe_dump
     feature :expiration
 
-    default_expiration 14.days
+    default_expiration 14.days # by default 2x Secret.default_expiration
     prefix :metadata
 
-    identifier_field :key
+    identifier_field :identifier
 
-    field :custid
+    field :owner_id
     field :state
-    field :key
-    field :secret_key
-    field :secret_shortkey
+    field :secret_identifier
     field :secret_ttl
     field :lifespan
     field :share_domain
     field :passphrase
-    field :viewed, fast_method: false
-    field :received, fast_method: false
-    field :shared, fast_method: false
-    field :burned, fast_method: false
     field :created
     field :updated
 
@@ -34,47 +27,9 @@ module Onetime
     # that based on the `secret_ttl` and the `created` timestamp. See
     # the secret_expired? and expiration methods.
     field :recipients
-    field :truncate # boolean
-
-    # NOTE: this field is a nullop. It's only populated if a value was entered
-    # into a hidden field which is something a regular person would not do.
-    field :token
-
-    # NOTE: Safe dump fields are loaded once at start time so they're
-    # immune to hot reloads.
-    safe_dump_field :identifier, ->(obj) { obj.identifier }
-    safe_dump_field :key
-    safe_dump_field :custid
-    safe_dump_field :state
-    safe_dump_field :secret_shortkey
-    safe_dump_field :secret_ttl
-    safe_dump_field :metadata_ttl, ->(m) { m.lifespan }
-    safe_dump_field :lifespan
-    safe_dump_field :share_domain
-    safe_dump_field :created
-    safe_dump_field :updated
-    safe_dump_field :shared
-    safe_dump_field :received
-    safe_dump_field :burned
-    safe_dump_field :viewed
-    safe_dump_field :recipients
-    safe_dump_field :shortkey, ->(m) { m.key.slice(0, 8) }
-    safe_dump_field :show_recipients, ->(m) { !m.recipients.to_s.empty? }
-    safe_dump_field :is_viewed, ->(m) { m.state?(:viewed) }
-    safe_dump_field :is_received, ->(m) { m.state?(:received) }
-    safe_dump_field :is_burned, ->(m) { m.state?(:burned) }
-    safe_dump_field :is_expired, ->(m) { m.state?(:expired) }
-    safe_dump_field :is_orphaned, ->(m) { m.state?(:orphaned) }
-    safe_dump_field :is_destroyed, lambda { |m|
-      m.state?(:received) || m.state?(:burned) || m.state?(:expired) || m.state?(:orphaned)
-    }
-    # We use the hash syntax here since `:truncated?` is not a valid symbol.
-    safe_dump_field :is_truncated, ->(m) { m.truncated? }
-    safe_dump_field :has_passphrase, ->(m) { m.has_passphrase? }
 
     def init
       self.state ||= 'new'
-      self.key   ||= self.class.generate_id # rubocop:disable Naming/MemoizedInstanceVariableName
     end
 
     def age
@@ -140,187 +95,6 @@ module Onetime
       !passphrase.to_s.empty?
     end
 
-    def deliver_by_email(cust, locale, secret, eaddrs, template = nil, ticketno = nil)
-      template ||= Onetime::Email::SecretLink
-
-      if eaddrs.nil? || eaddrs.empty?
-        secret_logger.info "No email addresses specified for delivery", {
-          metadata_key: key,
-          secret_key: secret.key,
-          user: cust.obscure_email,
-          action: 'deliver_email'
-        }
-        return
-      end
-
-      secret_logger.debug "Preparing email delivery", {
-        metadata_key: key,
-        secret_key: secret.key,
-        user: cust.obscure_email,
-        token: token.nil? ? nil : 'present',
-        action: 'deliver_email'
-      }
-
-      eaddrs = [eaddrs].flatten.compact[0..9] # Max 10
-
-      eaddrs_safe     = eaddrs.collect { |e| OT::Utils.obscure_email(e) }
-      eaddrs_safe_str = eaddrs_safe.join(', ')
-
-      secret_logger.info "Delivering secret by email", {
-        metadata_key: key,
-        secret_key: secret.key,
-        user: cust.obscure_email,
-        recipient_count: eaddrs_safe.size,
-        recipients: eaddrs_safe_str,
-        action: 'deliver_email'
-      }
-      recipients! eaddrs_safe_str
-
-      if eaddrs.size > 1
-        secret_logger.warn "Multiple recipients detected", {
-          metadata_key: key,
-          secret_key: secret.key,
-          recipient_count: eaddrs.size,
-          action: 'deliver_email'
-        }
-      end
-
-      eaddrs.each do |email_address|
-        view                  = template.new cust, locale, secret, email_address
-        view.ticketno         = ticketno if ticketno
-        view.emailer.reply_to = cust.email
-        view.deliver_email token # pass the token from spawn_pair through
-        break # force just a single recipient
-      end
-    end
-
-    # NOTE: We override the default fast writer (bang!) methods from familia
-    # so that we can update two fields at once. To replicate the same behavior
-    # we pass update_expiration: false to save so that changing this metdata
-    # objects state doesn't affect its original expiration time.
-    #
-    # TODO: Replace with transaction (i.e. MULTI/EXEC command)
-    def viewed!
-      # A guard to allow only a fresh, new secret to be viewed. Also ensures
-      # that we don't support going from viewed back to something else.
-      return unless state?(:new)
-
-      self.state  = 'viewed'
-      self.viewed = Familia.now.to_i
-      # The nuance bewteen being "viewed" vs "received" or "burned" is
-      # that the secret link page has been requested (via GET)
-      # but the "View Secret" button hasn't been clicked yet (i.e. we haven't
-      # yet received the POST request that actually reveals the contents
-      # of the secret). It's a subtle but important distinction bc it
-      # communicates an amount of activity around the secret. The terminology
-      # can be improved though and we'll also want to achieve parity with the
-      # API by allowing a GET (or OPTIONS) for the secret as a check that it
-      # is still valid -- that should set the state to viewed as well.
-      save update_expiration: false
-
-      secret_logger.info "Metadata state transition to viewed", {
-        metadata_key: shortkey,
-        secret_key: secret_shortkey,
-        previous_state: 'new',
-        new_state: 'viewed',
-        timestamp: viewed
-      }
-    end
-
-    def received!
-      # A guard to allow only a fresh secret to be received. Also ensures
-      # that we don't support going from received back to something else.
-      return unless state?(:new) || state?(:viewed)
-
-      previous_state = state
-      self.state      = 'received'
-      self.received   = Familia.now.to_i
-      self.secret_key = ''
-      save update_expiration: false
-
-      secret_logger.info "Metadata state transition to received", {
-        metadata_key: shortkey,
-        secret_key: secret_shortkey,
-        previous_state: previous_state,
-        new_state: 'received',
-        timestamp: received
-      }
-    end
-
-    # We use this method in special cases where a metadata record exists with
-    # a secret_key value but no valid secret object exists. This can happen
-    # when a secret is manually deleted but the metadata record is not. Otherwise
-    # it's a bug and although unintentional we want to handle it gracefully here.
-    def orphaned!
-      # A guard to prevent modifying metadata records that already have
-      # cleared out the secret (and that probably have already set a reason).
-      return if secret_key.to_s.empty?
-      return unless state?(:new) || state?(:viewed) # only new or viewed secrets can be orphaned
-
-      previous_state = state
-      self.state      = 'orphaned'
-      self.updated    = Familia.now.to_i
-      self.secret_key = ''
-      save update_expiration: false
-
-      secret_logger.warn "Metadata state transition to orphaned", {
-        metadata_key: shortkey,
-        secret_key: secret_shortkey,
-        previous_state: previous_state,
-        new_state: 'orphaned',
-        timestamp: updated
-      }
-    end
-
-    def burned!
-      # See guard comment on `received!`
-      return unless state?(:new) || state?(:viewed)
-
-      previous_state = state
-      self.state      = 'burned'
-      self.burned     = Familia.now.to_i
-      self.secret_key = ''
-      save update_expiration: false
-
-      secret_logger.info "Metadata state transition to burned", {
-        metadata_key: shortkey,
-        secret_key: secret_shortkey,
-        previous_state: previous_state,
-        new_state: 'burned',
-        timestamp: burned
-      }
-    end
-
-    def expired!
-      # A guard to prevent prematurely expiring a secret. We only want to
-      # expire secrets that are actually old enough to be expired.
-      return unless secret_expired?
-
-      previous_state = state
-      self.state      = 'expired'
-      self.updated    = Familia.now.to_i
-      self.secret_key = ''
-      save update_expiration: false
-
-      secret_logger.info "Metadata state transition to expired", {
-        metadata_key: shortkey,
-        secret_key: secret_shortkey,
-        previous_state: previous_state,
-        new_state: 'expired',
-        timestamp: updated,
-        secret_ttl: secret_ttl,
-        age_seconds: age
-      }
-    end
-
-    def state?(guess)
-      state.to_s == guess.to_s
-    end
-
-    def truncated?
-      truncate.to_s == 'true'
-    end
-
     def load_secret
       Onetime::Secret.load secret_key
     end
@@ -329,6 +103,41 @@ module Onetime
       def generate_id
         Familia.generate_id
       end
+
+      # def save_secret
+      #   secret.encrypt_value secret_value
+      #   metadata.default_expiration             = ttl * 2
+      #   secret.default_expiration               = ttl
+      #   metadata.lifespan        = metadata.default_expiration.to_i
+      #   metadata.secret_ttl      = secret.default_expiration.to_i
+      #   metadata.secret_shortkey = secret.shortkey
+      #   metadata.share_domain    = share_domain
+      #   secret.lifespan          = secret.default_expiration.to_i
+      #   secret.share_domain      = share_domain
+      #   secret.save
+      #   metadata.save
+      #   @greenlighted            = metadata.valid? && secret.valid?
+      # end
+
+      def spawn_pair(owner_id, lifespan, content, passphrase: nil, domain: nil)
+
+        secret   = Onetime::Secret.new
+        metadata = Onetime::Metadata.new(owner_id: owner_id)
+
+        metadata.secret_identifier = secret.objid
+        metadata.default_expiration = lifespan
+        metadata.save
+
+        secret.default_expiration = lifespan
+        secret.metadata_identifier = metadata.objid
+        secret.domain = domain # transient fields need to be populated before
+        secret.passphrase = passphrase # encrypting the content fio aad protection
+        secret.ciphertext = content
+        secret.save
+
+        [metadata, secret]
+      end
     end
+
   end
 end
