c96e3b87cfa804d55d9d3004e160bc9ee80a809a
delano <delano@onetimesecret.com>
2025-11-05 19:17:45 -0800

    [#1872] Expand QuietLayout width to 4xl

--- src/layouts/QuietLayout.vue
+++ src/layouts/QuietLayout.vue
@@ -1,8 +1,8 @@
 <!-- src/layouts/QuietLayout.vue -->
 <script setup lang="ts">
+  import { WindowService } from '@/services/window.service';
   import type { LayoutProps } from '@/types/ui/layouts';
   import BaseLayout from './BaseLayout.vue';
-  import { WindowService } from '@/services/window.service';
 
   const sitHost = WindowService.get('site_host') ?? null;
   const props = withDefaults(defineProps<LayoutProps>(), {});
@@ -24,7 +24,7 @@
     </template>
     <template #main>
       <main
-        class="container mx-auto max-w-2xl p-4"
+        class="container mx-auto max-w-4xl p-4"
         name="QuietLayout">
         <slot></slot>
       </main>
@@ -37,13 +37,13 @@
       <!-- Powered By Link -->
       <div
         v-if="displayPoweredBy"
-        class="mt-8 mb-4 text-center">
+        class="mb-4 mt-8 text-center">
         <a
           :href="`https://${sitHost}`"
-          class="text-[0.7rem] text-gray-300 transition-colors duration-200 hover:text-gray-400 dark:text-gray-600 dark:hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
+          class="text-[0.7rem] text-gray-300 transition-colors duration-200 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 dark:text-gray-600 dark:hover:text-gray-500"
           rel="noopener noreferrer"
           :aria-label="$t('web.homepage.visit-onetime-secret-home')">
-          {{$t('powered-by-onetime-secret')}}
+          {{ $t('powered-by-onetime-secret') }}
         </a>
       </div>
     </template>
