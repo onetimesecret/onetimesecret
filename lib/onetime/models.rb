1be1a19eb86f3a23a1596a5e0272ee761ccb89e7
delano <delano@onetimesecret.com>
2025-11-07 00:51:02 -0800

    [#1884] Fix for org->team->customer load order

--- lib/onetime/models.rb
+++ lib/onetime/models.rb
@@ -4,7 +4,7 @@ require_relative 'models/features'
 require_relative 'models/metadata'
 require_relative 'models/secret'
 require_relative 'models/organization'
+require_relative 'models/team'
 require_relative 'models/customer'
-require_relative 'models/team'  # Must load after Customer (participates_in dependency)
 require_relative 'models/custom_domain'
 require_relative 'models/feedback'
