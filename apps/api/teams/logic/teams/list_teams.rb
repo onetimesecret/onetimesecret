6f9d001ac9aaad6e62123ee2500eff95701b9f91
delano <delano@onetimesecret.com>
2025-11-07 19:21:50 -0800

    [#1884] Update to Familia pre22

--- apps/api/teams/logic/teams/list_teams.rb
+++ apps/api/teams/logic/teams/list_teams.rb
@@ -17,19 +17,14 @@ module TeamAPI::Logic
       def process
         OT.ld "[ListTeams] Listing teams for user #{cust.custid}"
 
-        # Get all teams where user is owner or member
-        # Note: This is a simple implementation. For large scale,
-        # consider adding a customer.teams relationship in the model
-        all_teams = Onetime::Team.values.members.map { |teamid| Onetime::Team.load(teamid) }.compact
-
-        # Filter to teams where current user is a member
-        @teams = all_teams.select { |team| team.member?(cust) }
+        @teams = cust.team_instances
 
         OT.ld "[ListTeams] Found #{@teams.size} teams"
 
         success_data
       end
 
+      # TODO: Replace with safe_dump
       def success_data
         {
           user_id: cust.objid,
