9d41a20e47d8ce0db9aaf431b88ffef6de7df776
delano <delano@onetimesecret.com>
2025-11-26 08:42:39 -0800

    Reduce purgatory surface area
    Moves some images back to public/web

--- .purgatory/apps-api-v2-controllers.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'controllers/account'
-require_relative 'controllers/colonel'
-require_relative 'controllers/domains'
-require_relative 'controllers/secrets'
-require_relative 'controllers/meta'
