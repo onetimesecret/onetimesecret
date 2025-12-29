76e8ad33831d694d22c863ff5ba4d987d1432825
delano <delano@onetimesecret.com>
2025-12-28 18:38:01 -0800

    [#2224] Remove .env.sh symlink

--- .env.sh
+++ .env.sh
@@ -1,1 +1,17 @@
-.env-local
+#!/usr/bin/env bash
+# .env.sh
+
+# A convenience wrapper so that .env is just a basic environment file
+# that is compatible everywhere while still using the auto-export
+# functionality the shell gods left for us.
+
+# set -a enables automatic export mode. All variable assignments between
+# here and and set +a will be exported to child processes without needing
+# 'export' keyword.
+set -a
+
+# Load the vars
+[ -f .env ] && . .env
+
+# Set +a restores default behavior where variables must be explicitly exported
+set +a
