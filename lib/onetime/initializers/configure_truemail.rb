1a7cf5107fea6ecc3562221e70a247ab1db8a332
delano <delano@onetimesecret.com>
2026-02-23 18:22:01 -0800

    [#ARGS-0] Symbolize Truemail logger options before assignment
    IndifferentHash yields string keys and YAML may parse symbol
    values like :error as plain strings when permitted_classes is
    not set. Convert keys and tracking_event to symbols before
    passing to Truemail's logger= setter.

--- lib/onetime/initializers/configure_truemail.rb
+++ lib/onetime/initializers/configure_truemail.rb
@@ -18,6 +18,15 @@ module Onetime
             OT.le "config.#{actual_key} does not exist"
             # next
           end
+          # Truemail's logger= expects symbol keys and symbol values
+          # (e.g. tracking_event: :error) but IndifferentHash yields
+          # string keys and YAML may parse `:error` as the string "error"
+          # when permitted_classes: [Symbol] is absent.
+          if actual_key == :logger && value.is_a?(Hash)
+            value = value.transform_keys(&:to_sym)
+            value[:tracking_event] = value[:tracking_event].to_sym if value[:tracking_event].is_a?(String)
+          end
+
           OT.ld "Setting Truemail config key #{sym_key} to #{value}"
           config.send("#{actual_key}=", value)
         end
