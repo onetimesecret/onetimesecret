f5f486c02e4deeb750ee7a886ab420ae08f38dd2
delano <delano@onetimesecret.com>
2026-03-24 21:56:03 -0700

    [#2761] Fix: Display customer email instead of ID
    The feedback form was incorrectly displaying the customer's external ID.
    This commit changes it to display the customer's email address, which is
    more relevant for support.

--- src/apps/secret/components/support/FeedbackForm.vue
+++ src/apps/secret/components/support/FeedbackForm.vue
@@ -185,7 +185,7 @@
                 stroke-width="2"
                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>
-            {{ t('web.account.customer_id') }}: {{ cust?.extid }}
+            {{ t('web.account.customer_id') }}: {{ cust?.email }}
           </li>
           <li class="flex items-center">
             <svg
