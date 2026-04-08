84eb408a5eb8c6274bd28ec335e4ad73d1880b2b
delano <delano@onetimesecret.com>
2026-04-07 19:32:36 -0700

    [#2929] Add heroicons-inbox-arrow-down to SVG sprite

--- src/shared/components/icons/sprites/HeroiconsSprites.vue
+++ src/shared/components/icons/sprites/HeroiconsSprites.vue
@@ -1795,6 +1795,17 @@ clip-rule="evenodd" /></symbol>
       </symbol>
       <symbol
         viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        id="heroicons-inbox-arrow-down">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+          d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+      </symbol>
+      <symbol
+        viewBox="0 0 24 24"
         id="heroicons-lock-closed-solid">
         <path
           fill="currentColor"
